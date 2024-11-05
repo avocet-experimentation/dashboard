c42530243a04270c887953d3d6d9fb576477b26a
Adnan Shihabi <adnan@ashihabi.me>
2024-11-04 18:49:30 -0800

    Restored FFlag table changes
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/FlagTable.tsx
+++ src/components/FlagTable.tsx
@@ -1,15 +1,64 @@
-import { useState, useEffect } from 'react';
-import { Flag } from '#/lib/types';
-import Table from './Table';
+import { useState } from "react";
+import { Table } from "@chakra-ui/react";
+import { Switch } from "./ui/switch";
+import { FeatureFlag } from "@fflags/types";
 
 interface FlagTableProps {
-  data: Flag[],
+  data: FeatureFlag[];
 }
 
+const handleEnvironmentSwitch = async (fflagId: string): Promise<void> => {
+  try {
+    const res = await fetch(`http://localhost:3524/admin/fflag/${fflagId}`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default function FlagTable({ data }: FlagTableProps) {
+  const [flags, setFlags] = useState(data);
   return (
-    <div className='flag-table-container'>
-      <Table data={data} />
-    </div>
+    <Table.Root className="table">
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>Feature Name</Table.ColumnHeader>
+          <Table.ColumnHeader>Dev</Table.ColumnHeader>
+          <Table.ColumnHeader>Prod</Table.ColumnHeader>
+          <Table.ColumnHeader>Testing</Table.ColumnHeader>
+          <Table.ColumnHeader>Default Value</Table.ColumnHeader>
+          <Table.ColumnHeader>Override Rules</Table.ColumnHeader>
+          <Table.ColumnHeader>Last Updated</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {flags &&
+          flags.map((datum: FeatureFlag) => (
+            <Table.Row key={datum.id}>
+              <Table.Cell>{datum.name}</Table.Cell>
+              <Table.Cell>
+                <Switch
+                  checked={datum.environments.dev.enabled}
+                  onCheckedChange={() => handleEnvironmentSwitch(datum.id)}
+                />
+              </Table.Cell>
+              <Table.Cell>
+                <Switch checked={datum.environments.prod.enabled} />
+              </Table.Cell>
+              <Table.Cell>
+                <Switch checked={datum.environments.testing.enabled} />
+              </Table.Cell>
+              <Table.Cell>{datum.defaultValue}</Table.Cell>
+              <Table.Cell>TBD</Table.Cell>
+              <Table.Cell>{datum.updatedAt}</Table.Cell>
+            </Table.Row>
+          ))}
+      </Table.Body>
+    </Table.Root>
   );
 }
