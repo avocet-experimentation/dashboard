7ad899b564f9a648381bb7688cffe41cc43ee790
Adnan Shihabi <adnan@ashihabi.me>
2024-11-04 16:14:02 -0800

    Rebuild NavBar with Chakra
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Route, Switch } from 'wouter';
-import './assets/stylesheets/App.css';
-import Navbar from './components/Navbar';
-import FlagTable from './components/FlagTable';
-import EventTable from './components/EventTable';
-import { Flag, Span } from './lib/types';
-import { useEffect, useState } from 'react';
-import './services/FlagService';
-import { exampleFlags, exampleSpans } from './services/exampleData';
-import EventService from './services/EventService';
+import { Route, Switch } from "wouter";
+// import "./assets/stylesheets/App.css";
+import Navbar from "./components/Navbar";
+import FlagTable from "./components/FlagTable";
+import EventTable from "./components/EventTable";
+import { Flag, Span } from "./lib/types";
+import { useEffect, useState } from "react";
+import "./services/FlagService";
+import { exampleFlags, exampleSpans } from "./services/exampleData";
+import EventService from "./services/EventService";
+import { Flex } from "@chakra-ui/react";
 
 export default function App() {
   const [flags, setFlags] = useState<Flag[]>([]);
@@ -22,18 +23,26 @@ export default function App() {
       if (allEvents) {
         setEvents(allEvents);
       }
-    }
+    };
 
     fetchData();
   }, []);
 
   return (
-    <div className='App'>
+    <Flex direction="row" width="100vw" height="100vh">
       <Navbar />
-      <Switch>
-        <Route path='/' component={() => <FlagTable data={flags} />} />
-        <Route path='/events' component={() => <EventTable data={events} />} />
-      </Switch>
-    </div>
+      <Flex direction="column" width="80%" bg="whitesmoke">
+        <Switch>
+          <Route
+            path="/features"
+            component={() => <FlagTable data={flags} />}
+          />
+          <Route
+            path="/events"
+            component={() => <EventTable data={events} />}
+          />
+        </Switch>
+      </Flex>
+    </Flex>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,82 @@
 /* for now, links to EventTable and FlagTable only
-*/
+ */
 
+import { Flex, chakra, Icon } from "@chakra-ui/react";
+import {
+  Flag,
+  Microscope,
+  TestTubes,
+  AlignHorizontalDistributeCenter,
+} from "lucide-react";
 import { Link } from "wouter";
 
-export default function Navbar() {
+const NavBox = chakra("div", {
+  base: {
+    width: "90%",
+    borderRadius: "5px",
+    color: "black",
+    height: "50px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    fontSize: "1.15em",
+    padding: "0 20px",
+    _hover: {
+      bg: "gray.200",
+    },
+  },
+});
 
+const NavText = chakra("p", {
+  base: {
+    color: "black",
+    textDecoration: "none",
+    padding: "15px",
+    width: "15cqi",
+  },
+});
+
+export default function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li><Link href='/'>Feature Flags</Link></li>
-        <li><Link href='/events'>Event Data</Link></li>
-      </ul>
-    </div>
+    <Flex
+      direction="column"
+      width="20%"
+      height="100%"
+      bg="white"
+      alignItems="center"
+    >
+      <NavBox>
+        <Icon>
+          <Flag />
+        </Icon>
+        <Link href="/features" draggable={false}>
+          <NavText>Feature Flags</NavText>
+        </Link>
+      </NavBox>
+      <NavBox>
+        <Icon>
+          <TestTubes />
+        </Icon>
+        <Link href="/experiments" draggable={false}>
+          <NavText>Experiments</NavText>
+        </Link>
+      </NavBox>
+      <NavBox>
+        <Icon>
+          <AlignHorizontalDistributeCenter />
+        </Icon>
+        <Link href="/events" draggable={false}>
+          <NavText>Event Data</NavText>
+        </Link>
+      </NavBox>
+      <NavBox>
+        <Icon>
+          <Microscope />
+        </Icon>
+        <Link href="/insights" draggable={false}>
+          <NavText>Insights</NavText>
+        </Link>
+      </NavBox>
+    </Flex>
   );
-};
+}
