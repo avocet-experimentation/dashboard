54854018c9dcaab7de4fb365b44808c8fa0fa230
Adnan Shihabi <adnan@ashihabi.me>
2024-11-04 19:32:12 -0800

    Force light mode.
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { Provider } from "./components/ui/provider.tsx";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/stylesheets/index.css";
+import { Theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <Theme appearance="light">
+        <App />
+      </Theme>
     </Provider>
   </React.StrictMode>
 );
