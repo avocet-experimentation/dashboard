c18cefcd30b5bc5396af5260da98d5e7567b2ad0
Adnan Shihabi <adnan@ashihabi.me>
2024-11-04 19:32:34 -0800

    Initialize Flag Form
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/FlagForm.tsx
+++ src/components/FlagForm.tsx
@@ -1,33 +1,21 @@
-import { FeatureFlag } from '#/lib/types';
-import { useState, useEffect } from 'react';
+import { Field, Input, Stack } from "@chakra-ui/react";
+import { FeatureFlag } from "@fflags/types";
+import { useState } from "react";
 
-const defaults = {
-  name: '',
-  description: '',
-  metrics: {
-    primary: '',
-    secondary: '',
-  },
-  state: ''
-}
 export default function FlagManagementForm({ flag }: { flag?: FeatureFlag }) {
   // populate fields with the flag object if passed
 
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [metrics, setMetrics] = useState();
-  const [status, setStatus] = useState('');
-  const [targetingRules, setTargetingRules] = useState([]);
-  const [environments, setEnvironments] = useState('');
-  const [name, setName] = useState('');
-  
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [environments, setEnvironments] = useState("");
+
   return (
-    <div>
-      <form id='flag-management-form'>
-        <input type='text' value={name}>
-        
-        </input>
-      </form>
-    </div>
+    <form id="flag-management-form">
+      <Stack gap="4">
+        <Field label="Flag Name">
+          <Input></Input>
+        </Field>
+      </Stack>
+    </form>
   );
 }
