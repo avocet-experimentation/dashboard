6ce7c6ff1198ce28ae330c80645dab474561952a
Adnan Shihabi <adnan@ashihabi.me>
2024-11-06 11:55:57 -0800

    Changes: - Restructured paths in `components` folder - File and component renaming - Fixed feature form data structure to match zod data type => correct submission structure led to a working `getAllFeatures` retreival.
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/experiments/ExperimentTable.tsx
+++ src/components/experiments/ExperimentTable.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { Table } from "@chakra-ui/react";
-import { Switch } from "./ui/switch";
-import { FeatureFlag } from "@fflags/types";
+import { Switch } from "../ui/switch";
 
-interface FlagTableProps {
-  data: FeatureFlag[];
-}
+// interface FlagTableProps {
+//   data: FeatureFlag[];
+// }
 
 const handleEnvironmentSwitch = async (fflagId: string): Promise<void> => {
   try {
@@ -21,38 +20,23 @@ const handleEnvironmentSwitch = async (fflagId: string): Promise<void> => {
   }
 };
 
-export default function FlagTable({ data }: FlagTableProps) {
-  const [flags, setFlags] = useState(data);
+export default function FlagTable({ data }) {
+  const [experiments, setExperiments] = useState(data);
   return (
-    <Table.Root className="table" >
+    <Table.Root className="table">
       <Table.Header>
         <Table.Row>
+          <Table.ColumnHeader>Experiment Name</Table.ColumnHeader>
           <Table.ColumnHeader>Feature Name</Table.ColumnHeader>
-          <Table.ColumnHeader>Dev</Table.ColumnHeader>
-          <Table.ColumnHeader>Prod</Table.ColumnHeader>
-          <Table.ColumnHeader>Testing</Table.ColumnHeader>
-          <Table.ColumnHeader>Default Value</Table.ColumnHeader>
           <Table.ColumnHeader>Override Rules</Table.ColumnHeader>
           <Table.ColumnHeader>Last Updated</Table.ColumnHeader>
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {flags &&
-          flags.map((datum: FeatureFlag) => (
+        {experiments &&
+          experiments.map((datum) => (
             <Table.Row key={datum.id}>
               <Table.Cell>{datum.name}</Table.Cell>
-              <Table.Cell>
-                <Switch
-                  checked={datum.environments.dev.enabled}
-                  onCheckedChange={() => handleEnvironmentSwitch(datum.id)}
-                />
-              </Table.Cell>
-              <Table.Cell>
-                <Switch checked={datum.environments.prod.enabled} />
-              </Table.Cell>
-              <Table.Cell>
-                <Switch checked={datum.environments.testing.enabled} />
-              </Table.Cell>
               <Table.Cell>{datum.defaultValue}</Table.Cell>
               <Table.Cell>TBD</Table.Cell>
               <Table.Cell>{datum.updatedAt}</Table.Cell>
