f55d01ad1df67b1cc10949aacadc01ee209b2d65
Adnan Shihabi <adnan@ashihabi.me>
2024-11-08 12:20:37 -0800

    Topbar component skeleton.
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+
+const TopBar = () => {
+  return (
+    <>
+      <Flex
+        direction="row"
+        background="white"
+        width="100%"
+        height="60px"
+        borderBottom="1px solid gray"
+      ></Flex>
+    </>
+  );
+};
+
+export default TopBar;
