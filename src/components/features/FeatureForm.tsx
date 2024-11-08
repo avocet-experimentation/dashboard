99ded69024c22935d3f457dc704b6a95c3079313
Adnan Shihabi <adnan@ashihabi.me>
2024-11-08 12:22:15 -0800

    Modified default value by emptying environment contents (besides the enabled prop). Modularized the `onSubmit` function by adding a `createFeature` function within the `FeatureService`.
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/features/FeatureForm.tsx
+++ src/components/features/FeatureForm.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import {
-  Button,
   chakra,
   createListCollection,
   Flex,
@@ -8,7 +6,6 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { Field } from "../ui/field";
 import {
   SelectContent,
   SelectItem,
@@ -16,10 +13,12 @@ import {
   SelectTrigger,
   SelectValueText,
 } from "../ui/select";
-import { FeatureFlag } from "@fflags/types";
-import { X } from "lucide-react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { Field } from "../ui/field";
 import { Switch } from "../ui/switch";
+import { useState } from "react";
+import { useForm, SubmitHandler, Controller, set } from "react-hook-form";
+import { FeatureFlag } from "@estuary/types";
+import FeatureService from "#/services/FeatureService";
 
 type Inputs = Omit<FeatureFlag, "id">;
 
@@ -36,116 +35,41 @@ const valueTypes = createListCollection({
 });
 
 const defaultFeatureFlag: Inputs = {
-  name: "New Feature Flag",
-  valueType: "string", // Defaulting to string, but adjust as needed
-  defaultValue: "", // Adjust based on default behavior for string type
-
+  name: "",
+  valueType: "boolean", // Defaulting to string, but adjust as needed
+  defaultValue: "true", // Adjust based on default behavior for string type
+  description: "",
   environments: {
     prod: {
       name: "prod",
       enabled: false,
-      overrideRules: [
-        {
-          id: "rule-1",
-          description: "Default production rule",
-          status: "draft",
-          enrollment: {
-            attributes: [
-              {
-                name: "attribute-name",
-                dataType: "string",
-              },
-            ],
-            proportion: 0.0,
-          },
-          startTimestamp: undefined,
-          endTimestamp: undefined,
-        },
-      ],
+      overrideRules: [],
     },
     dev: {
       name: "dev",
       enabled: true,
-      overrideRules: [
-        {
-          id: "rule-1",
-          description: "Default development rule",
-          status: "draft",
-          enrollment: {
-            attributes: [
-              {
-                name: "attribute-name",
-                dataType: "string",
-              },
-            ],
-            proportion: 0.0,
-          },
-          startTimestamp: undefined,
-          endTimestamp: undefined,
-        },
-      ],
+      overrideRules: [],
     },
     testing: {
       name: "testing",
       enabled: true,
-      overrideRules: [
-        {
-          id: "rule-1",
-          description: "Default testing rule",
-          status: "draft",
-          enrollment: {
-            attributes: [
-              {
-                name: "attribute-name",
-                dataType: "string",
-              },
-            ],
-            proportion: 0.0,
-          },
-          startTimestamp: undefined,
-          endTimestamp: undefined,
-        },
-      ],
+      overrideRules: [],
     },
     staging: {
-      name: "Staging",
+      name: "staging",
       enabled: false,
-      overrideRules: [
-        {
-          id: "rule-1",
-          description: "Default staging rule",
-          status: "draft",
-          enrollment: {
-            attributes: [
-              {
-                name: "attribute-name",
-                dataType: "string",
-              },
-            ],
-            proportion: 0.0,
-          },
-          startTimestamp: undefined,
-          endTimestamp: undefined,
-        },
-      ],
+      overrideRules: [],
     },
   },
-  description: "A new feature flag for experimentation",
   createdAt: Date.now(),
   updatedAt: Date.now(),
 };
 
-// const handleValueType = (value: string | string[]): string => {
-//   if (typeof value === "string") {
-//     return value;
-//   } else if (typeof value === "object") {
-//     return value[0];
-//   }
-//   return "";
-// };
+const featureService = new FeatureService();
 
-const FeatureCreationForm = ({ setShowForm }) => {
+const FeatureCreationForm = ({ formId }) => {
   const [valueType, setValueType] = useState("boolean");
+  const [isError, setIsError] = useState(null);
   const {
     control,
     register,
@@ -154,188 +78,164 @@ const FeatureCreationForm = ({ setShowForm }) => {
   } = useForm<Inputs>({
     defaultValues: defaultFeatureFlag,
   });
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log("data", data);
-    try {
-      const res = await fetch("http://localhost:3524/admin/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          mode: "cors",
-        },
-        body: JSON.stringify(data),
-      });
-    } catch (error) {
-      console.log(error);
+  const onSubmit: SubmitHandler<Inputs> = async (featureContent) => {
+    const response = await featureService.createFeature(featureContent);
+    if (response.status === 409) {
+      const { error } = await response.json();
+      setIsError(error);
+    } else if (response.status === 201) {
+      const featureId = response.text();
     }
   };
 
   return (
-    <Flex
-      direction="column"
-      bg="white"
-      borderRadius="5px"
-      width="40%"
-      height="55vh"
-      position="absolute"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      padding="25px"
-    >
-      <Flex
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        bg="whitesmoke"
-      >
-        Create New Feature
-        <X cursor="pointer" onClick={() => setShowForm(false)} />
-      </Flex>
-      <chakra.form id="flag-management-form" onSubmit={handleSubmit(onSubmit)}>
-        <Stack gap="4">
+    <chakra.form id={formId} onSubmit={handleSubmit(onSubmit)}>
+      <Stack gap="4">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <Field
+              label="Feature Name"
+              invalid={!!errors.name}
+              errorText={errors.name?.message}
+            >
+              <Input
+                placeholder="my-first-flag"
+                {...register("name", {
+                  required:
+                    "Feature name is required and must be between 3-20 characters long.",
+                  minLength: 3,
+                  maxLength: 20,
+                })}
+              />
+            </Field>
+          )}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <Field
+              label="Description"
+              invalid={!!errors.name}
+              errorText={errors.description?.message}
+            >
+              <Input
+                placeholder="A human-readable description of your feature flag."
+                {...register("description", {
+                  required: "A description of your feature is required.",
+                })}
+              />
+            </Field>
+          )}
+        />
+        <Field label="Enabled Environments">
+          <Flex direction="row" width="100%" justifyContent="space-evenly">
+            {environments.items.map((env) => (
+              <Controller
+                key={env}
+                name={`environments.${env}.enabled`}
+                control={control}
+                render={({ field }) => (
+                  <Flex>
+                    <Text marginRight="5px">{`${env}:`}</Text>
+                    <Switch
+                      id={env}
+                      name={field.name}
+                      checked={!!field.value}
+                      onCheckedChange={({ checked }) => field.onChange(checked)}
+                      inputProps={{ onBlur: field.onBlur }}
+                      width="fit-content"
+                    />
+                  </Flex>
+                )}
+              />
+            ))}
+          </Flex>
+        </Field>
+        <Field label="Value Type" width="320px">
           <Controller
-            name="name"
+            control={control}
+            name="valueType"
+            render={({ field }) => (
+              <SelectRoot
+                name={field.name}
+                value={[field.value]}
+                onValueChange={({ value }) => {
+                  field.onChange(value[0]);
+                  setValueType(value[0]);
+                }}
+                onInteractOutside={() => field.onBlur()}
+                collection={valueTypes}
+              >
+                <SelectTrigger>
+                  <SelectValueText />
+                </SelectTrigger>
+                <SelectContent zIndex="popover">
+                  {valueTypes.items.map((type) => (
+                    <SelectItem item={type} key={type.value}>
+                      {type.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
+            )}
+          />
+        </Field>
+        {valueType === "boolean" && (
+          <Controller
+            name="defaultValue"
             control={control}
             render={({ field }) => (
-              <Field label="Feature Name">
+              <Switch
+                name={field.name}
+                checked={!!field.value}
+                onCheckedChange={({ checked }) => field.onChange(checked)}
+                inputProps={{ onBlur: field.onBlur }}
+              >
+                {!!field.value ? "on" : "off"}
+              </Switch>
+            )}
+          />
+        )}
+        {valueType === "string" && (
+          <Controller
+            name="defaultValue"
+            control={control}
+            render={({ field }) => (
+              <Field label="Default Value">
                 <Input
-                  placeholder="my-first-flag"
-                  {...register("name", {
-                    required: "Feature name is required.",
+                  type="text"
+                  placeholder="A string value"
+                  {...register("defaultValue", {
+                    required: "A default value is required.",
                   })}
                 />
               </Field>
             )}
           />
+        )}
+        {valueType === "number" && (
           <Controller
-            name="description"
+            name="defaultValue"
+            defaultValue={0}
             control={control}
             render={({ field }) => (
-              <Field label="Description">
+              <Field label="Default Value">
                 <Input
-                  placeholder="A human-readable description of your feature flag."
-                  {...register("description", {
-                    required: "A description of your feature is required.",
+                  type="number"
+                  placeholder="A number value"
+                  {...register("defaultValue", {
+                    required: "A default value is required.",
                   })}
                 />
               </Field>
             )}
           />
-          <Field label="Enabled Environments">
-            <Flex direction="row" width="100%" justifyContent="space-evenly">
-              {environments.items.map((env) => (
-                <Controller
-                  key={env}
-                  name={`environments.${env}.enabled`}
-                  control={control}
-                  render={({ field }) => (
-                    <Flex position="relative">
-                      <Text marginRight="5px">{`${env}:`}</Text>
-                      <Switch
-                        value={field.value}
-                        name={field.name}
-                        checked={field.value}
-                        onCheckedChange={({ checked }) =>
-                          field.onChange(checked)
-                        }
-                        inputProps={{ onBlur: field.onBlur }}
-                      />
-                    </Flex>
-                  )}
-                />
-              ))}
-            </Flex>
-          </Field>
-          <Field label="Value Type" width="320px">
-            <Controller
-              control={control}
-              name="valueType"
-              render={({ field }) => {
-                console.log(field.value[0]);
-                return (
-                  <SelectRoot
-                    name={field.name}
-                    value={field.value}
-                    onValueChange={({ value }) => {
-                      field.onChange(value[0]);
-                      setValueType(value[0]);
-                    }}
-                    onInteractOutside={() => field.onBlur()}
-                    collection={valueTypes}
-                  >
-                    <SelectTrigger>
-                      <SelectValueText />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {valueTypes.items.map((type) => (
-                        <SelectItem item={type} key={type.value}>
-                          {type.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </SelectRoot>
-                );
-              }}
-            />
-          </Field>
-          {valueType === "boolean" && (
-            <Controller
-              name="defaultValue"
-              control={control}
-              render={({ field }) => (
-                <Switch
-                  name={field.name}
-                  checked={field.value}
-                  onCheckedChange={({ checked }) => field.onChange(checked)}
-                  inputProps={{ onBlur: field.onBlur }}
-                >
-                  {field.value ? "on" : "off"}
-                </Switch>
-              )}
-            />
-          )}
-          {valueType === "string" && (
-            <Controller
-              name="defaultValue"
-              control={control}
-              render={({ field }) => (
-                <Field label="Default Value">
-                  <Input
-                    type="text"
-                    placeholder="A string value"
-                    {...register("defaultValue", {
-                      required: "A default value is required.",
-                    })}
-                  />
-                </Field>
-              )}
-            />
-          )}
-          {valueType === "number" && (
-            <Controller
-              name="defaultValue"
-              defaultValue={0}
-              control={control}
-              render={({ field }) => (
-                <Field label="Default Value">
-                  <Input
-                    type="number"
-                    placeholder="A number value"
-                    {...register("defaultValue", {
-                      required: "A default value is required.",
-                    })}
-                  />
-                </Field>
-              )}
-            />
-          )}
-          <Button color="black" size="sm" type="submit" mt="4">
-            Create
-          </Button>
-        </Stack>
-      </chakra.form>
-    </Flex>
+        )}
+      </Stack>
+    </chakra.form>
   );
 };
 export default FeatureCreationForm;
