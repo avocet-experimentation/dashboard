8443677ee5bd771a915f45d3e6e8389e0b0e286f
Adnan Shihabi <adnan@ashihabi.me>
2024-11-11 12:34:57 -0800

    Restructuring:
    Routes are more reasonable. Removed data fetching from the App component; they should be done within their respective components to reduce amount of unnecessary requests.
    
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Provider } from "./components/ui/provider.tsx";
+import { defaultSystem } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/stylesheets/index.css";
-import { Theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <Theme appearance="light">
-        <App />
-      </Theme>
+      <App />
     </Provider>
   </React.StrictMode>
 );
