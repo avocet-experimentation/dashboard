ec39614f91d284207913d9dcadb6b01acfa065ae
Adnan Shihabi <adnan@ashihabi.me>
2024-11-11 12:48:42 -0800

    Page changes:
    - Implemented method to get all features
    - Addressed FormModal restructure
    - Added message for when there are no features in the DB
    
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,11 +1,32 @@
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import { CirclePlus } from "lucide-react";
 import FeatureCreationForm from "./FeatureForm";
-import FormModalTrigger from "../Modal";
+import FormModalTrigger from "../FormModal";
+import FeatureService from "#/services/FeatureService";
+import { FeatureFlag } from "@estuary/types";
+import { useEffect, useState } from "react";
+import FeatureTable from "./FeatureTable";
 
 const CREATE_FEATURE_FORM_ID = "create-feature-form";
 
-const Features = ({ children }) => {
+const featureService = new FeatureService();
+
+const Features = () => {
+  const [features, setFeatures] = useState<FeatureFlag[]>([]);
+
+  useEffect(() => {
+    const handleGetAllFeatures = async () => {
+      try {
+        const allFeatures = await featureService.getAllFeatures();
+        setFeatures(allFeatures ? allFeatures : []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return () => handleGetAllFeatures();
+  }, []);
+
   return (
     <Flex direction="column" padding="25px">
       <Flex
@@ -19,15 +40,23 @@ const Features = ({ children }) => {
           triggerButtonIcon={<CirclePlus />}
           triggerButtonText={"Add Feature"}
           title={"Create a New Feature"}
-          form={<FeatureCreationForm formId={CREATE_FEATURE_FORM_ID} />}
           formId={CREATE_FEATURE_FORM_ID}
           confirmButtonText={"Create"}
-        />
+        >
+          <FeatureCreationForm
+            formId={CREATE_FEATURE_FORM_ID}
+            setIsLoading={undefined}
+          />
+        </FormModalTrigger>
       </Flex>
-      <Text margin="25px 0">
+      <Text margin="15px 0">
         Features enable you to change your app's behavior from within this UI.
       </Text>
-      {children}
+      {features.length ? (
+        <FeatureTable features={features} />
+      ) : (
+        "No features found. Please create one."
+      )}
     </Flex>
   );
 };
