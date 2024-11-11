f4a684116fb9b0cbdb22658ef89fbf9125f0fc52
Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
2024-11-11 12:44:08 -0800

    added additional query methods

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -7,10 +7,39 @@ export default class EventService {
 
   async getAllEvents() {
     try {
+      console.log(this.baseUrl)
       const response = await fetch(this.baseUrl + '/events');
+      console.log(response)
       const allEvents = (await response.json())._rs.rows;
       console.log({allEvents})
       return allEvents;
+    } catch(e: unknown) {
+      if (e instanceof Error) {
+        console.error(e);
+      }
+    }
+  }
+
+  async getAllEventTypes() {
+    try {
+      const response = await fetch(this.baseUrl + '/uniqueEvents');
+      const eventTypes = Object.values((await response.json()));
+      console.log(eventTypes);
+      return eventTypes;
+    } catch(e: unknown) {
+      if (e instanceof Error) {
+        console.error(e);
+      }
+    }
+  }
+
+  async getEventsOfType(eventType: String) {
+    try {
+      // console.log(`eventType: ${eventType}`);
+      const response = await fetch(this.baseUrl + `/eventType/${eventType}`);
+      const events = (await response.json()).rows;
+      // console.log(events);
+      return events;
     } catch(e: unknown) {
       if (e instanceof Error) {
         console.error(e);
