a59bf78f1765fd4e67f95a7cc20c30eeee25f446
Adnan Shihabi <adnan@ashihabi.me>
2024-11-13 13:15:21 -0800

    Form additions:
    Added traffic, group, and variation configuration to the form.
    
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/components/experiments/ExperimentForm.tsx
+++ src/components/experiments/ExperimentForm.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import {
+  Box,
   Button,
   chakra,
-  createListCollection,
   Flex,
+  IconButton,
   Input,
   Stack,
 } from "@chakra-ui/react";
+import { Table } from "@chakra-ui/react";
 import { Field } from "../ui/field";
+import { CirclePlus, CircleEqual } from "lucide-react";
+
 import {
   SelectContent,
   SelectItem,
@@ -15,123 +19,356 @@ import {
   SelectTrigger,
   SelectValueText,
 } from "../ui/select";
-import { X } from "lucide-react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { Experiment } from "@fflags/types";
+import {
+  useForm,
+  SubmitHandler,
+  Controller,
+  useFieldArray,
+} from "react-hook-form";
+import { Experiment, ExperimentGroup } from "@estuary/types";
+import { Slider } from "../ui/slider";
+
+// type Inputs = Omit<Experiment, "id" | "groups"> & {
+//   groups: Omit<ExperimentGroup, "id">[];
+// };
 
 type Inputs = Omit<Experiment, "id">;
 
+const defaultExperiment: Inputs = {
+  name: "",
+  description: "",
+  type: "Experiment",
+  status: "draft",
+  enrollment: {
+    attributes: ["id"],
+    proportion: 100, // this will be converted `onSubmit` to a value between 0 and 1
+  },
+  groups: [
+    {
+      id: "",
+      name: "Control",
+      proportion: 0.5,
+      blocks: [
+        {
+          id: "0",
+          name: "",
+          flagValue: true,
+        },
+      ],
+    },
+    {
+      id: "",
+      name: "Variation",
+      proportion: 0.5,
+      blocks: [
+        {
+          id: "1",
+          name: "",
+          flagValue: true,
+        },
+      ],
+    },
+  ],
+  flagId: "",
+  dependents: [],
+};
+
+const appendedGroup = (index: number): ExperimentGroup => {
+  return {
+    id: "",
+    name: `Variation ${index}`,
+    proportion: 0,
+    blocks: [
+      {
+        id: "0",
+        name: "",
+        flagValue: true,
+      },
+    ],
+  };
+};
+
+const setEqualProportions = (
+  fields: ExperimentGroup[],
+  fieldArrayUpdate
+): void => {
+  const numOfVariations: number = fields.length;
+  const split: number = Math.trunc((1 / numOfVariations) * 10000) / 10000;
+  let remainder = 1 - split * numOfVariations;
+  console.log(split, remainder);
+  fields.forEach((_, index) => {
+    let refinedSplit = split;
+    if (remainder > 0) {
+      refinedSplit += 0.0001;
+      remainder -= 0.0001;
+    }
+    fieldArrayUpdate(index, {
+      ...fields[index],
+      proportion: refinedSplit,
+    });
+  });
+};
+
+const createGroupIds = (expContent: Inputs) => {
+  const expName = expContent.name;
+  const expGroups = expContent.groups;
+  expGroups.forEach((group) => {
+    const groupName = group.name;
+    group.id = `${expName}-${groupName}`;
+  });
+};
+
+const reformatAllTrafficProportion = (expContent: Inputs): void => {
+  const originalProportion = expContent.enrollment.proportion;
+  const reformatted = parseFloat((originalProportion / 100).toFixed(2));
+  expContent.enrollment.proportion = reformatted;
+};
+
 const ExperimentCreationForm = ({ formId, setIsLoading }) => {
-  const [allFeatures, setAllFeatures] = useState(
-    createListCollection({
-      items: [{ name: "test-flag", id: "abc123" }],
-    })
-  );
+  const [render, setRender] = useState();
   const {
     control,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>({
-    defaultValues: {
-      name: "",
-      groups: [{ id: "blah", name: "control", proportion: 0.5 }],
-    },
+    defaultValues: defaultExperiment,
   });
-  const onSubmit = (data) => {
-    console.log("data", data);
+
+  const { fields, append, remove, update } = useFieldArray({
+    control,
+    name: "groups",
+  });
+
+  const onSubmit = (expContent: Inputs) => {
+    createGroupIds(expContent);
+    reformatAllTrafficProportion(expContent);
+    console.log("data", expContent);
   };
 
   useEffect(() => {});
 
   return (
-    <Flex
-      direction="column"
-      bg="white"
-      borderRadius="5px"
-      width="40%"
-      height="55vh"
-      position="absolute"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      padding="25px"
+    <chakra.form
+      id="experiment-management-form"
+      onSubmit={handleSubmit(onSubmit)}
     >
-      <Flex
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        bg="whitesmoke"
-      >
-        Create New Experiment
-        <X cursor="pointer" onClick={() => setShowForm(false)} />
-      </Flex>
-      <chakra.form id="flag-management-form" onSubmit={handleSubmit(onSubmit)}>
-        <Stack gap="4">
+      <Stack gap="4">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <Field
+              label="Experiment Name"
+              helperText={
+                !errors.name
+                  ? "Acts as a unique identifier used to track impressions and analyze results."
+                  : null
+              }
+              invalid={!!errors.name}
+              errorText={errors.name?.message}
+            >
+              <Input
+                placeholder="my-first-experiment"
+                {...register("name", {
+                  required:
+                    "Experiment name is required and must be between 3-20 characters long.",
+                  pattern: {
+                    value: /^[0-9A-Za-z-]+$/gi,
+                    message:
+                      "Experiment names may only contain letters, numbers, and hyphens.",
+                  },
+                  minLength: 3,
+                  maxLength: 20,
+                })}
+              />
+            </Field>
+          )}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <Field label="Description">
+              <Input
+                placeholder="A human-readable description of your experiment."
+                {...register("description", {
+                  required: "A description of your experiment is required.",
+                })}
+              />
+            </Field>
+          )}
+        />
+        {/* <Controller
+          name="hypothesis"
+          control={control}
+          render={({ field }) => (
+            <Field label="Hypothesis">
+              <Input
+                placeholder="What do you expect to happen in this experiment ?"
+                {...register("hypothesis", {
+                  required: "A hypothesis of your experiment is required.",
+                })}
+              />
+            </Field>
+          )}
+        /> */}
+        {/* <Field
+          label="Assign value based on attribute"
+          helperText="Will be hashed together with the Tracking Key to determine which variation to assign."
+        >
           <Controller
-            name="name"
             control={control}
-            render={({ field }) => (
-              <Field label="Feature Name">
-                <Input
-                  placeholder="my-first-experiment"
-                  {...register("name", {
-                    required: "Experiment name is required.",
-                  })}
-                />
-              </Field>
-            )}
+            name="featureFlag"
+            render={({ field }) => {
+              return (
+                <SelectRoot
+                  name={field.name}
+                  value={field.value}
+                  onValueChange={({ value }) => {
+                    field.onChange(value[0]);
+                  }}
+                  onInteractOutside={() => field.onBlur()}
+                  collection={allFeatures}
+                >
+                  <SelectTrigger>
+                    <SelectValueText />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {allFeatures.items.map((feature) => (
+                      <SelectItem item={feature.name} key={feature.id}>
+                        {feature.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </SelectRoot>
+              );
+            }}
           />
-          <Controller
-            name="hypothesis"
-            control={control}
-            render={({ field }) => (
-              <Field label="Description">
-                <Input
-                  placeholder="What is your testing goal with this experiment?"
-                  {...register("hypothesis", {
-                    required: "A hypothesis of your experiment is required.",
-                  })}
-                />
-              </Field>
-            )}
-          />
-          <Field label="Value Type" width="320px">
-            <Controller
-              control={control}
-              name="featureFlag"
-              render={({ field }) => {
-                return (
-                  <SelectRoot
-                    name={field.name}
-                    value={field.value}
-                    onValueChange={({ value }) => {
-                      field.onChange(value[0]);
-                    }}
-                    onInteractOutside={() => field.onBlur()}
-                    collection={allFeatures}
-                  >
-                    <SelectTrigger>
-                      <SelectValueText />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {allFeatures.items.map((feature) => (
-                        <SelectItem item={feature.name} key={feature.id}>
-                          {feature.name}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </SelectRoot>
-                );
-              }}
-            />
-          </Field>
-          <Button color="black" size="sm" type="submit" mt="4">
-            Create
-          </Button>
-        </Stack>
-      </chakra.form>
-    </Flex>
+        </Field> */}
+        <Controller
+          name="enrollment.proportion"
+          control={control}
+          render={({ field }) => (
+            <Field label={"Traffic included in this Experiment"}>
+              <Slider
+                width="full"
+                onFocusChange={({ focusedIndex }) => {
+                  if (focusedIndex !== -1) return;
+                  field.onBlur();
+                }}
+                name={field.name}
+                value={[field.value]}
+                onValueChange={({ value }) => {
+                  field.onChange(value);
+                }}
+              />
+            </Field>
+          )}
+        />
+        <Controller
+          name="groups"
+          control={control}
+          render={({ field }) => (
+            <Field
+              label={"Groups"}
+              invalid={!!errors.value?.length}
+              errorText={errors.value?.[0]?.message}
+            >
+              <Box maxHeight="250px" overflowY="auto" width="100%">
+                <Table.Root stickyHeader>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.ColumnHeader>VARIATION</Table.ColumnHeader>
+                      <Table.ColumnHeader>NAME</Table.ColumnHeader>
+                      <Table.ColumnHeader>SPLIT</Table.ColumnHeader>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                    {fields.map((field, index) => (
+                      <Table.Row key={field.id}>
+                        <Table.Cell></Table.Cell>
+                        <Table.Cell>
+                          <Input
+                            border="0px"
+                            width="125px"
+                            defaultValue={field.name || `Variation ${index}`}
+                            {...register(`groups.${index}.name`, {
+                              required:
+                                "Feature name is required and must be between 3-20 characters long.",
+                              pattern: {
+                                value: /^[0-9A-Za-z-]+$/gi,
+                                message:
+                                  "Feature names may only contain letters, numbers, and hyphens.",
+                              },
+                              minLength: 3,
+                              maxLength: 20,
+                            })}
+                          />
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Field
+                            invalid={!!errors.groups?.[index]?.proportion}
+                            errorText={
+                              errors.groups?.[index]?.proportion?.message
+                            }
+                          >
+                            <Input
+                              border="0px"
+                              width="75px"
+                              defaultValue={field.proportion || 0}
+                              {...register(`groups.${index}.proportion`, {
+                                required: "Group proportion is required.",
+                                valueAsNumber: true,
+                                min: {
+                                  value: 0,
+                                  message: ">= 0",
+                                },
+                                max: {
+                                  value: 1,
+                                  message: "<= 1",
+                                },
+                              })}
+                            />
+                          </Field>
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                  </Table.Body>
+                </Table.Root>
+              </Box>
+              <Flex
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                width="100%"
+              >
+                <Button
+                  border="0px"
+                  variant="plain"
+                  background="transparent"
+                  _hover={{ backgroundColor: "transparent", color: "blue" }}
+                  onClick={() => append(appendedGroup(fields.length))}
+                >
+                  <CirclePlus />
+                  Add variation
+                </Button>
+                <Button
+                  border="0px"
+                  variant="plain"
+                  background="transparent"
+                  _hover={{ backgroundColor: "transparent", color: "blue" }}
+                  onClick={() => setEqualProportions(fields, update)}
+                >
+                  <CircleEqual />
+                  Set equal proportions
+                </Button>
+              </Flex>
+            </Field>
+          )}
+        />
+      </Stack>
+    </chakra.form>
   );
 };
 export default ExperimentCreationForm;
