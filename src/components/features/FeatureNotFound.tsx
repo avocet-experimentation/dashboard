4cf7da37f23b27fdedabfeed14f674e4010f0947
Adnan Shihabi <adnan@ashihabi.me>
2024-11-15 15:05:02 -0800

    Feature Not Found page
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/components/features/FeatureNotFound.tsx
+++ src/components/features/FeatureNotFound.tsx
@@ -1,13 +1,18 @@
-import { Flex, Text } from "@chakra-ui/react";
-import { SearchX } from "lucide-react";
+import { Flex, chakra, Text } from "@chakra-ui/react";
+import SearchX from "../../assets/svgs/search-x.svg";
 
 const FeatureNotFound = () => {
   return (
-    <Flex direction="column" justifyContent="center" alignItems="center">
-      <Flex direction="row">
-        <SearchX />
-        <Text>The feature you are looking for does not exist.</Text>
-      </Flex>
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
+    >
+      <chakra.img src={SearchX} width="150px" draggable={false} />
+      <Text fontSize="3xl">
+        The feature you are looking for does not exist.
+      </Text>
     </Flex>
   );
 };
