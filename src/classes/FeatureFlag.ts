05d1feff4df4de5b6e28ff0f8064480008a52be1
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-15 15:00:48 -0800

    Classes for creating feature flags, experiments, and templates for switchback and A/B
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/classes/FeatureFlag.ts
@@ -0,0 +1,27 @@
+import { FeatureFlagDraft, FlagEnvironmentMapping, FlagValueDef } from "@estuary/types";
+import { FlagValueDefTemplate, FlagEnvironmentMappingTemplate } from "./FeatureFlagSubclasses";
+
+export class FeatureFlagDraftImpl implements FeatureFlagDraft {
+  name: string;
+  description?: string;
+  value: FlagValueDef;
+  environments: FlagEnvironmentMapping;
+
+  constructor({ name, description, value, environments}: FeatureFlagDraft) {
+    this.name = name;
+    this.description = description;
+    this.value = value;
+    this.environments = environments;
+  }
+}
+
+export class FeatureFlagDraftTemplate extends FeatureFlagDraftImpl {
+  constructor(name: string, dataType: "string" | "number" | "boolean") {    
+    const defaults = {
+      value: new FlagValueDefTemplate(dataType) as FlagValueDef,
+      environments: new FlagEnvironmentMappingTemplate(),
+    }
+
+    super({ name, ...defaults });
+  }
+}
