ee3c896cc86ba896bac20b4b187daa5eec2f7bf2
Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
2024-11-15 16:59:14 -0800

    added several methods for filtering spans
    Co-Authored-By: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-Authored-By: Adnan Shihabi <adnan@ashihabi.me>
    Co-Authored-By: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-Authored-By: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

---
+++ src/lib/hash.ts
@@ -0,0 +1,119 @@
+import { ClientPropMapping, ExperimentGroup, Treatment, Experiment } from '@estuary/types';
+import crypto from 'node:crypto';
+/*
+MD5 vs DJB2:
+- MD5 is more collision resistant so it is prefereable to be used for larget data sets.
+- DJB2 is faster and more lightweight, as it relies on bitwise operations and some arithmetic. Ideal for frequent hashing. MD5 is has greater computation costs
+- MD5 produces a larger range of values (128bit hash) (less collision resistance) than DJB2 (32bit hash)
+
+- MD5 is better suited where more collision resistance is needed, DJB2 is ideal for fast hash computations
+- MD5 adds more dependency by use of crypto module, DJB2 has no depdencies.
+
+*/
+
+// function hashIdentifiers <T extends boolean | string | number> (identifiers: { [attributeName: string]: string }, flagValues: Set<T>): T {
+//
+
+
+/**
+ * DJB2 Hash function.
+ * @param input 
+ * @returns a signed 32-bit integer
+ */
+export function hashStringDJB2(input: string) {
+  let hash = 0;
+  // iterate over the string, use bitwise left shift operator, which is essentially multiply the value by 32 -- increases significance of the current hash value.
+  // subtract the hash from the result
+  // Add the utf char value
+  for (let i = 0; i < input.length; i++) {
+    hash = (hash << 5) - hash + input.charCodeAt(i); 
+    // console.log('left shifted:', hash)
+    hash |= 0; // Convert to 32bit integer -- bitwise OR operator(?)
+    // console.log('converted to 32 bit:', hash)
+  }
+
+  return hash;
+}
+
+/**
+ * Combines a collection of strings presumed to be unique IDs.
+ * Used for creating a hash of experiment, group, and block ID for sending to client
+ */
+export function combineIds(ids: readonly string[]): string {
+  const sortedIds = ids.toSorted();
+  const combined = sortedIds.join('');
+  return combined;
+}
+
+export function hashStringSet(strings: readonly string[]) {
+  const combined = combineIds(strings);
+  return hashStringDJB2(combined);
+}
+
+export function hashIdentifiers(identifierMap: ClientPropMapping) {
+  const identifiers = Object.entries(identifierMap);
+  const sortedIdentifiers = identifiers.toSorted((a, b) => (a[0] > b[0]) ? 1 : -1);
+  let string = '';
+
+  sortedIdentifiers.forEach(([ name, value ]) => {
+    string += name + value;
+  });
+
+  return hashStringDJB2(string);
+}
+
+/**
+ * Hash identifiers for pseudo-random assignment to one of many options
+ * todo:
+ * - change assignment options to Array<{ id: string, weight: proportion }>
+ * @param identifiers An array of values to use collectively as a unique identifier for the client
+ * @param assignmentOptions An array of IDs for possible assignments
+ * @returns one of the options passed in
+ */
+export function hashAndAssign(identifiers: ClientPropMapping, assignmentOptions: readonly string[]): string {
+  const hash = hashIdentifiers(identifiers);
+  const sortedOptions = [...assignmentOptions].sort();
+  const index = Math.abs(hash) % sortedOptions.length;
+  return sortedOptions[index];
+}
+
+export const getExpHash = (
+  experiment: Experiment,
+  group: ExperimentGroup,
+  treatment: Treatment
+) => hashStringSet([experiment.id, group.id, treatment.id]);
+
+// const flagValues = ['active', 'inactive', 'pending', 'suspended'];
+
+// console.log(hashAndAssign({z: 'z',  az: 'a', ab: 'ab', bc: 'b', email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues) === hashAndAssign({ email: 'sean.a.mentele@gmail.com', name: 'sean', z: 'z',  az: 'a', ab: 'ab', bc: 'b'}, flagValues));
+// console.log(hashAndAssign({name: 'sean', email: 'sean.a.mentele@gmail.com'}, flagValues) ===  hashAndAssign({email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues))
+// console.log(hashAndAssign({userId: "12345", userName: "john_doe"}, flagValues));
+// console.log(hashAndAssign({name: 'sean', email: 'sean.a.mentele@gmail.com'}, flagValues));
+
+// function hashIdentifiersMD5(identifiers: Identifier[]) {
+//   const sortedKeys = Object.keys(identifiers).sort();
+//   let string = '';
+
+//   sortedKeys.forEach((key) => {
+//     string += identifiers[key];
+//   });
+  
+//   //Create a new MD5 hash instance -> Produces 128bit hash value (32char hexadecimal string)
+//   const hexString = crypto.createHash('md5')
+//     .update(string) //Feed the input string into the hash instance
+//     .digest('hex'); //Specify hex to return a hexidecimal string
+//   const hash = BigInt('0x' + hexString); //Converts to BigInt
+
+//   return hash;
+// }
+
+// function hashAndAssignMD5(identifiers: Identifier[], flagValues=['Control', 'Variant']) {
+//   const hashInt = hashIdentifiersMD5(identifiers);
+//   const flagArray = Array.from(flagValues).sort();
+
+//   const index = Number(hashInt % BigInt(flagArray.length))
+
+//   return flagArray[index];
+// }
+
+// console.log(hashAndAssignMD5({z: 'z',  az: 'a', ab: 'ab', bc: 'b', email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues));
---
+++ src/lib/spanFilters.ts
@@ -0,0 +1,67 @@
+/*
+	- [ ] Filters we want on the dashboard:
+		- [X] spans containing any flag data
+		- [X] spans containing a given flag name
+		- [ ] spans containing a given experiment (get the hash using `expIdHash` of all blocks for that experiment and query for matches)
+		- [ ] spans containing a given experiment group (same, but just blocks in the group)
+	- [ ] getting field data from filtered spans:
+		- [X] get all values for a given field on span attributes
+			- [ ] in dashboard, allow users to provide a field name and its data type (eg., `"<appname>.order.subtotal"`, `number`) to define the dependent variable on an experiment
+- [ ] Aggregations on span types
+	- [ ] total count of data per experiment group | just use length after getting exp group?
+	- [ ] sum of some user-chosen property per experiment group | length on exp group -> get all vals
+*/
+
+import * as hashLib from './hash';
+import { ClientPropMapping, ExperimentGroup, Treatment, Experiment } from '@estuary/types';
+
+const flagVariantRegex = /estuary-exp\.\S{3,}\.variant/;
+const flagHashRegex = /estuary-exp\.\S{3,}\.hash/;
+
+const spansWithFlags = (spans) => {
+  return spans.filter(span => {
+    const keys = Object.keys(span.spanattributes)
+    let hasFlag = false;
+    keys.forEach(key => {
+      if (key.match(flagVariantRegex)) {
+        hasFlag = true;
+      }
+    });
+    
+    return hasFlag;
+  }); // if spans exists, return true
+}
+
+// span.spanattributes.spans is an Object with flag keys in this format:
+// [`estuary-exp.${flagName}.variant`]: String(flag.value),
+// [`estuary-exp.${flagName}.hash`]: String(flag.hash),
+const spansWithFlagName = (spans, flagName: String) => {
+  return spans.filter(span => {
+    return `estuary-exp.${flagName}.variant` in span.spanattributes;
+  });
+}
+
+const spansForExperiment = (spans, experiment: Experiment, group: ExperimentGroup, treatment: Treatment) => {
+  const targetHash = `"${hashLib.getExpHash(experiment, group, treatment)}"`; // '"1495033474"'
+  return spans.filter(span => {
+    let match = false;
+    Object.keys(span.spanattributes).forEach(key => {
+      console.log(span.spanattributes[key] )
+      if (span.spanattributes[key] === targetHash) { match = true }
+    });
+
+    return match;
+  });
+}
+
+    /*
+    const expHashes = Object.keys(span.spanattributes)
+      .filter(attr => attr.match(flagHashRegex));
+    */
+
+// get an array of the value of a given spanattributes property for each span
+const getPropertyValueArray = (spans, property) => {
+  return spans.map(span => span.spanattributes[property]);
+} 
+
+export { spansWithFlags, spansWithFlagName, spansForExperiment, getPropertyValueArray }
