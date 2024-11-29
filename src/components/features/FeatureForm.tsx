327aefa8555040f1860cb6a9f1b7e2233d825860
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-29 15:45:18 -0800

    Feature flag component changes: - Can create feature flags of any type - Some fields under FeatureForm moved into their own components for readability - FeatureForm data is validated using flag draft schema before attempting to create the document - Can create override rules from FeaturePage - Rule creation form being refactored to show a different form depending on the rule type - Rules are validated against schema when rule creation form is submitted - Feature flag list displays toggle status on the set of all environments on all fetched flags
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/components/features/FeatureForm.tsx
+++ src/components/features/FeatureForm.tsx
@@ -1,47 +1,35 @@
 import {
-  chakra,
   createListCollection,
   Flex,
   Input,
   Stack,
   Text,
-} from "@chakra-ui/react";
-import {
-  SelectContent,
-  SelectItem,
-  SelectRoot,
-  SelectTrigger,
-  SelectValueText,
-} from "../ui/select";
-import { Field } from "../ui/field";
-import { Switch } from "../ui/switch";
-import { useState } from "react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod"
+} from '@chakra-ui/react';
+import { Field } from '../ui/field';
+import { Switch } from '../ui/switch';
+import { useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   FeatureFlagDraft,
   featureFlagDraftSchema,
-  flagDefaultValueMap,
-  flagValueTypeDefSchema,
   SchemaParseError,
-} from "@estuary/types";
-import FeatureService from "#/services/FeatureService";
-import { useLocation } from "wouter";
+} from '@estuary/types';
+import FeatureService from '#/services/FeatureService';
+import { useLocation } from 'wouter';
+import FeatureFlagValueTypeField from './FeatureFlagValueTypeField';
+import FeatureFlagDefaultValueField from './FeatureFlagDefaultValueField';
 
 // todo: replace this placeholder with fetched environment names
 const environments = createListCollection<string>({
-  items: ["dev", "prod", "testing", "staging"],
+  items: ['dev', 'prod', 'testing', 'staging'],
 });
 const valueTypes = createListCollection({
   items: [
-    { label: "Boolean (on/off)", value: "boolean" },
-    { label: "String", value: "string" },
-    { label: "Number", value: "number" },
+    { label: 'Boolean (on/off)', value: 'boolean' },
+    { label: 'String', value: 'string' },
+    { label: 'Number', value: 'number' },
   ],
-});
-
-const defaultFeatureFlag: FeatureFlagDraft = FeatureFlagDraft.templateBoolean({
-  name: ''
 });
 
 const featureService = new FeatureService();
@@ -51,9 +39,10 @@ interface FeatureFlagCreationFormProps {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function FeatureCreationForm(
-  { formId, setIsLoading }: FeatureFlagCreationFormProps
-) {
+export default function FeatureCreationForm({
+  formId,
+  setIsLoading,
+}: FeatureFlagCreationFormProps) {
   const [isError, setIsError] = useState(null);
   const [location, navigate] = useLocation();
 
@@ -65,33 +54,35 @@ export default function FeatureCreationForm(
     getValues,
     formState: { errors },
   } = useForm<FeatureFlagDraft>({
-    defaultValues: defaultFeatureFlag,
+    defaultValues: FeatureFlagDraft.templateBoolean({
+      name: '',
+    }),
     // resolver: zodResolver(featureFlagDraftSchema),
   });
 
   const onSubmit: SubmitHandler<FeatureFlagDraft> = async (featureContent) => {
     console.log('submit handler invoked');
-    
+
     // placeholder
     Object.keys(featureContent.environmentNames).forEach((key) => {
       if (featureContent.environmentNames[key] !== true) {
         delete featureContent.environmentNames[key];
       }
-  })
+    });
+    console.log({ featureContent });
     // these lines unnecessary if the resolver argument to `useForm` works
     const safeParseResult = featureFlagDraftSchema.safeParse(featureContent);
-    console.log({featureContent})
     if (!safeParseResult.success) {
       // the error pretty-print the Zod parse error message
-      throw new SchemaParseError(safeParseResult); 
+      throw new SchemaParseError(safeParseResult);
     }
     setIsLoading(true);
     const response = await featureService.createFeature(safeParseResult.data);
     if (response.status === 409) {
       const { error } = await response.json();
       setIsError(error);
-    } else if (response.status === 201) {
-      const { fflagId } = await response.json();
+    } else if (response.ok) {
+      const { fflagId } = response.body;
       navigate(`/features/${fflagId}`);
     } else if (response.status) {
     }
@@ -99,52 +90,52 @@ export default function FeatureCreationForm(
   };
 
   return (
-    <chakra.form id={formId} onSubmit={handleSubmit(onSubmit)}>
+    <form id={formId} onSubmit={handleSubmit(onSubmit)}>
       <Stack gap="4">
-        <Controller
-          name="name"
-          control={control}
-          render={() => (
-            <Field
-              label="Feature Name"
-              invalid={!!errors.name}
-              errorText={errors.name?.message}
-            >
+        <Field
+          label="Feature Name"
+          invalid={!!errors.name}
+          errorText={errors.name?.message}
+        >
+          <Controller
+            name="name"
+            control={control}
+            render={() => (
               <Input
                 placeholder="my-first-flag"
-                {...register("name", {
+                {...register('name', {
                   required:
-                    "Feature name is required and must be between 3-20 characters long.",
+                    'Feature name is required and must be between 3-20 characters long.',
                   pattern: {
                     value: /^[0-9A-Za-z-]+$/gi,
                     message:
-                      "Feature names may only contain letters, numbers, and hyphens.",
+                      'Feature names may only contain letters, numbers, and hyphens.',
                   },
                   minLength: 3,
                   maxLength: 20,
                 })}
               />
-            </Field>
-          )}
-        />
-        <Controller
-          name="description"
-          control={control}
-          render={() => (
-            <Field
-              label="Description"
-              invalid={!!errors.description}
-              errorText={errors.description?.message}
-            >
+            )}
+          />
+        </Field>
+        <Field
+          label="Description"
+          invalid={!!errors.description}
+          errorText={errors.description?.message}
+        >
+          <Controller
+            name="description"
+            control={control}
+            render={() => (
               <Input
                 placeholder="A human-readable description of your feature flag."
-                {...register("description", {
-                  required: "A description of your feature is required.",
+                {...register('description', {
+                  required: 'A description of your feature is required.',
                 })}
               />
-            </Field>
-          )}
-        />
+            )}
+          />
+        </Field>
         <Field label="Enabled Environments" as="p"></Field>
         <Flex direction="row" width="100%" justifyContent="space-evenly">
           {environments.items.map((env: string) => {
@@ -171,84 +162,18 @@ export default function FeatureCreationForm(
           })}
         </Flex>
 
-        <Field label="Value Type" width="320px">
-          <Controller
-            control={control}
-            name="value.type"
-            render={({ field }) => (
-              <SelectRoot
-                name={field.name}
-                value={[field.value]}
-                collection={valueTypes}
-                onValueChange={({ value }) => {
-                  const selectedValueType = flagValueTypeDefSchema.parse(value[0]);
-                  field.onChange(value[0]);
-                  const newDefault = flagDefaultValueMap[selectedValueType];
-                  console.log({selectedValueType, newDefault})
-                  setValue('value.initial', newDefault);
-                }}
-                onInteractOutside={() => field.onBlur()}
-              >
-                <SelectTrigger>
-                  <SelectValueText />
-                </SelectTrigger>
-                <SelectContent zIndex="popover">
-                  {valueTypes.items.map((type) => (
-                    <SelectItem item={type} key={type.value}>
-                      {type.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </SelectRoot>
-            )}
-          />
-        </Field>
-  
-        <Controller
-          name="value.initial"
+        <FeatureFlagValueTypeField
           control={control}
-          render={({ field }) => {
-            const valueType = getValues('value.type');
-            if (valueType === "boolean")
-              return (
-                <Switch
-                  name={field.name}
-                  checked={!!field.value}
-                  onCheckedChange={({ checked }) => field.onChange(checked)}
-                  inputProps={{ onBlur: field.onBlur }}
-                >
-                  {!!field.value ? "on" : "off"}
-                </Switch>
-              );
-            if (valueType === "string")
-              return (
-                <Field label="Default Value">
-                  <Input
-                    type="text"
-                    placeholder="A string value"
-                    {...register("value.initial", {
-                      required: "A default value is required.",
-                    })}
-                  />
-                </Field>
-              );
-            if (valueType === "number")
-              return (
-                <Field label="Default Value">
-                  <Input
-                    type="number"
-                    placeholder="A number value"
-                    {...register("value.initial", {
-                      valueAsNumber: true,
-                      required: "A default value is required.",
-                    })}
-                  />
-                </Field>
-              );
-            return <></>;
-          }}
+          setValue={setValue}
+          valueTypes={valueTypes}
+        />
+
+        <FeatureFlagDefaultValueField
+          control={control}
+          getValues={getValues}
+          register={register}
         />
       </Stack>
-    </chakra.form>
+    </form>
   );
-};
+}
--- src/components/features/FeaturePage.tsx
+++ src/components/features/FeaturePage.tsx
@@ -1,4 +1,4 @@
-import FeatureService from "#/services/FeatureService";
+import FeatureService from '#/services/FeatureService';
 import {
   Box,
   Editable,
@@ -10,15 +10,15 @@ import {
   IconButton,
   Stack,
   Text,
-} from "@chakra-ui/react";
-import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "../ui/menu";
-import { Switch } from "../ui/switch";
-import { useEffect, useState } from "react";
-import { FeatureFlag, FeatureFlagDraft } from "@estuary/types";
-import { Check, EllipsisVertical, FilePenLine, Trash2, X } from "lucide-react";
-import { useLocation, useRoute } from "wouter";
-import NotFound from "../NotFound";
-import EnvironmentTabs from "./EnvironmentTabs";
+} from '@chakra-ui/react';
+import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from '../ui/menu';
+import { Switch } from '../ui/switch';
+import { useEffect, useState } from 'react';
+import { FeatureFlag, FeatureFlagDraft } from '@estuary/types';
+import { Check, EllipsisVertical, FilePenLine, Trash2, X } from 'lucide-react';
+import { useLocation, useRoute } from 'wouter';
+import NotFound from '../NotFound';
+import EnvironmentTabs from './EnvironmentTabs';
 
 const featureService = new FeatureService();
 
@@ -28,13 +28,14 @@ export default function FeaturePage() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [editDesc, setEditDesc] = useState<boolean>(false);
   const [feature, setFeature] = useState<FeatureFlag | null>(null);
-  const [environments, setEnvironments] = useState<FeatureFlag['environmentNames']>();
-  const [match, params] = useRoute("/features/:id");
+  const [environments, setEnvironments] =
+    useState<FeatureFlag['environmentNames']>();
+  const [match, params] = useRoute('/features/:id');
   const [location, navigate] = useLocation();
 
   // todo: consider passing a prop in instead of using route params
   if (params === null) {
-    throw new Error(`Missing 'id' param for component FeaturePage!`)
+    throw new Error(`Missing 'id' param for component FeaturePage!`);
   }
 
   useEffect(() => {
@@ -47,7 +48,6 @@ export default function FeaturePage() {
         } else {
           setFeature(response.body);
           setEnvironments(response.body.environmentNames);
-
         }
       } catch (error) {
         console.log(error);
@@ -62,180 +62,185 @@ export default function FeaturePage() {
   const handleDeleteFeature = () => {
     if (feature) {
       featureService.deleteFeature(feature.id);
-      navigate("/features");
+      navigate('/features');
     }
   };
 
-  // if (isLoading) return <></>;
-
-  // if (feature) {
-    return (<>
-      {!isLoading &&
-        feature ? 
-      <Stack gap={4} padding="25px" overflowY="scroll">
-        <Flex justifyContent="space-between">
-          <Heading size="3xl">{feature.name}</Heading>
-          <MenuRoot>
-            <MenuTrigger asChild>
-              <IconButton size="md">
-                <EllipsisVertical color="black" />
-              </IconButton>
-            </MenuTrigger>
-            <MenuContent>
-              <MenuItem
-                value="delete"
-                valueText="Delete"
-                cursor="pointer"
-                color="fg.error"
-                _hover={{ bg: "bg.error", color: "fg.error" }}
-                onClick={handleDeleteFeature}
-              >
-                <Trash2 />
-                <Box flex="1">Delete</Box>
-              </MenuItem>
-            </MenuContent>
-          </MenuRoot>
-        </Flex>
-        <Box>
-          <Heading size="xl" marginBottom="15px">
-            Overview
-          </Heading>
-          <Stack padding="15px" bg="white" borderRadius="5px">
-            <HStack gap={2.5}>
-              <Heading size="lg">Description</Heading>
-              <Icon
-                size="sm"
-                cursor="pointer"
-                onClick={() => setEditDesc(true)}
-              >
-                <FilePenLine color="black" />
-              </Icon>
-            </HStack>
-            <Editable.Root
-              value={feature.description ?? ''}
-              edit={editDesc}
-              activationMode="focus"
-              onBlur={() => {
-                // setEditDesc(false);
-              }}
-              onSubmit={(event) => {
-                console.log("submit");
-                featureService.patchFeature(feature.id, {
-                  description: event.target.value,
-                });
-              }}
-            >
-              <Editable.Preview
-                minH="48px"
-                alignItems="flex-start"
-                width="full"
-              />
-              <Editable.Textarea />
-              <Editable.Control>
-                <Editable.CancelTrigger asChild>
-                  <IconButton variant="outline" size="xs">
-                    <X />
-                  </IconButton>
-                </Editable.CancelTrigger>
-                <Editable.SubmitTrigger asChild>
-                  <IconButton variant="outline" size="xs">
-                    <Check />
-                  </IconButton>
-                </Editable.SubmitTrigger>
-              </Editable.Control>
-            </Editable.Root>
-          </Stack>
-        </Box>
-        <Box>
-          <Heading size="xl" marginBottom="15px">
-            Enabled Environments
-          </Heading>
-          <Stack padding="15px" bg="white" borderRadius="5px">
-            <Flex>
-              <Text>
-                <Highlight
-                  query={["null"]}
-                  styles={{ color: "red", fontFamily: VALUE_FONT }}
+  return (
+    <>
+      {!isLoading && feature ? (
+        <Stack gap={4} padding="25px" overflowY="scroll">
+          <Flex justifyContent="space-between">
+            <Heading size="3xl">{feature.name}</Heading>
+            <MenuRoot>
+              <MenuTrigger asChild>
+                <IconButton size="md">
+                  <EllipsisVertical color="black" />
+                </IconButton>
+              </MenuTrigger>
+              <MenuContent>
+                <MenuItem
+                  value="delete"
+                  valueText="Delete"
+                  cursor="pointer"
+                  color="fg.error"
+                  _hover={{ bg: 'bg.error', color: 'fg.error' }}
+                  onClick={handleDeleteFeature}
                 >
-                  In a disabled environment, the feature will always evaluate to
-                  null. The default value and override rules will be ignored.
-                </Highlight>
-              </Text>
-            </Flex>
-            <Flex direction="row">
-              {environments && Object.keys(environments).map((env) => {
-                return (
-                  <Flex
-                    position="relative"
-                    margin="0 15px 0 0"
-                    key={`${env}-switch`}
-                  >
-                    <Text marginRight="5px">{env}:</Text>
-                    <Switch
-                      checked={env in environments}
-                      onCheckedChange={async ({ checked }) => {
-                        const toggleResult = await featureService.toggleEnvironment(feature.id, env);
-                        if (toggleResult.ok) {
-                          setEnvironments((prevState) => {
-                            const prevEnv = prevState ?? {};
-                            const prevFlag: FeatureFlagDraft = { ...feature, environmentNames: prevEnv };
-                            if (checked === true) {
-                              FeatureFlagDraft.enableEnvironment(prevFlag, env);
-                            } else {
-                              FeatureFlagDraft.disableEnvironment(prevFlag, env);
-
-                            }
-                            return prevFlag.environmentNames;
-                          });
-                        }
-                      }}
-                    />
-                  </Flex>
-                );
-              })}
-            </Flex>
-          </Stack>
-        </Box>
-
-        <Box>
-          <Heading size="xl" marginBottom="15px">
-            Values and Rules
-          </Heading>
-          <Stack padding="15px" bg="white" borderRadius="5px">
-            <Heading size="lg">Default Value</Heading>
-            <Flex border="1px solid gray" borderRadius="5px" padding="15px">
-              <Text fontWeight="bold" width="fit-content" padding="0 5px">
-                {feature.value.type.toUpperCase()}
-              </Text>
-              <Text
-                fontWeight="normal"
-                fontFamily={VALUE_FONT}
-                padding="0 10px"
-              >
-                {String(feature.value.initial)}
-              </Text>
-            </Flex>
-
-            <Heading size="lg" margin="15px 0 0 0">
-              Rules
+                  <Trash2 />
+                  <Box flex="1">Delete</Box>
+                </MenuItem>
+              </MenuContent>
+            </MenuRoot>
+          </Flex>
+          <Box>
+            <Heading size="xl" marginBottom="15px">
+              Overview
             </Heading>
-            <Text>
-              Add powerful logic on top of your feature. The first matching rule
-              applies and overrides the default value.
-            </Text>
-            <EnvironmentTabs
-              {...feature}
-              // featureId={params.id}
-              // environmentNames={environments}
-              // valueType={feature.value.type}
-              // defaultValue={feature.value.initial}
-            />
-          </Stack>
-        </Box>
-      </Stack>
-      : <NotFound componentName={"feature"} />}
-      </>);
-  // } else {
-  //   return <NotFound componentName={"feature"} />;
-  // }
-};
+            <Stack padding="15px" bg="white" borderRadius="5px">
+              <HStack gap={2.5}>
+                <Heading size="lg">Description</Heading>
+                <Icon
+                  size="sm"
+                  cursor="pointer"
+                  onClick={() => setEditDesc(true)}
+                >
+                  <FilePenLine color="black" />
+                </Icon>
+              </HStack>
+              <Editable.Root
+                value={feature.description ?? ''}
+                edit={editDesc}
+                activationMode="focus"
+                onBlur={() => {
+                  // setEditDesc(false);
+                }}
+                onSubmit={(event) => {
+                  console.log('submit');
+                  featureService.patchFeature(feature.id, {
+                    description: event.target.value,
+                  });
+                }}
+              >
+                <Editable.Preview
+                  minH="48px"
+                  alignItems="flex-start"
+                  width="full"
+                />
+                <Editable.Textarea />
+                <Editable.Control>
+                  <Editable.CancelTrigger asChild>
+                    <IconButton variant="outline" size="xs">
+                      <X />
+                    </IconButton>
+                  </Editable.CancelTrigger>
+                  <Editable.SubmitTrigger asChild>
+                    <IconButton variant="outline" size="xs">
+                      <Check />
+                    </IconButton>
+                  </Editable.SubmitTrigger>
+                </Editable.Control>
+              </Editable.Root>
+            </Stack>
+          </Box>
+          <Box>
+            <Heading size="xl" marginBottom="15px">
+              Enabled Environments
+            </Heading>
+            <Stack padding="15px" bg="white" borderRadius="5px">
+              <Flex>
+                <Text>
+                  <Highlight
+                    query={['null']}
+                    styles={{ color: 'red', fontFamily: VALUE_FONT }}
+                  >
+                    In a disabled environment, the feature will always evaluate
+                    to null. The default value and override rules will be
+                    ignored.
+                  </Highlight>
+                </Text>
+              </Flex>
+              <Flex direction="row">
+                {environments &&
+                  Object.keys(environments).map((env) => {
+                    return (
+                      <Flex
+                        position="relative"
+                        margin="0 15px 0 0"
+                        key={`${env}-switch`}
+                      >
+                        <Text marginRight="5px">{env}:</Text>
+                        <Switch
+                          checked={env in environments}
+                          onCheckedChange={async ({ checked }) => {
+                            const toggleResult =
+                              await featureService.toggleEnvironment(
+                                feature.id,
+                                env,
+                              );
+                            if (toggleResult.ok) {
+                              setEnvironments((prevState) => {
+                                const prevEnv = prevState ?? {};
+                                const prevFlag: FeatureFlagDraft = {
+                                  ...feature,
+                                  environmentNames: prevEnv,
+                                };
+                                if (checked === true) {
+                                  FeatureFlagDraft.enableEnvironment(
+                                    prevFlag,
+                                    env,
+                                  );
+                                } else {
+                                  FeatureFlagDraft.disableEnvironment(
+                                    prevFlag,
+                                    env,
+                                  );
+                                }
+                                return prevFlag.environmentNames;
+                              });
+                            }
+                          }}
+                        />
+                      </Flex>
+                    );
+                  })}
+              </Flex>
+            </Stack>
+          </Box>
+
+          <Box>
+            <Heading size="xl" marginBottom="15px">
+              Values and Rules
+            </Heading>
+            <Stack padding="15px" bg="white" borderRadius="5px">
+              <Heading size="lg">Default Value</Heading>
+              <Flex border="1px solid gray" borderRadius="5px" padding="15px">
+                <Text fontWeight="bold" width="fit-content" padding="0 5px">
+                  {feature.value.type.toUpperCase()}
+                </Text>
+                <Text
+                  fontWeight="normal"
+                  fontFamily={VALUE_FONT}
+                  padding="0 10px"
+                >
+                  {String(feature.value.initial)}
+                </Text>
+              </Flex>
+
+              <Heading size="lg" margin="15px 0 0 0">
+                Rules
+              </Heading>
+              <Text>
+                Add powerful logic on top of your feature. The first matching
+                rule applies and overrides the default value.
+              </Text>
+              <EnvironmentTabs {...feature} />
+            </Stack>
+          </Box>
+        </Stack>
+      ) : (
+        <NotFound componentName={'feature'} />
+      )}
+    </>
+  );
+}
