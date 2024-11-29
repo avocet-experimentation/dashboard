0bccf8ea48db3fa4dfe27c910fd2fee0f3b53281
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-29 15:58:27 -0800

    Sort environment names in feature flag table so they display in a consistent order
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/components/features/FeatureTable.tsx
+++ src/components/features/FeatureTable.tsx
@@ -1,45 +1,51 @@
 // components
-import { Table, Text } from "@chakra-ui/react";
-import { Switch } from "../ui/switch";
-import { Tooltip } from "../ui/tooltip";
+import { Table, Text } from '@chakra-ui/react';
+import { Switch } from '../ui/switch';
+import { Tooltip } from '../ui/tooltip';
 
 // library
-import { lastUpdated, formatDate } from "#/lib/timeFunctions";
+import { lastUpdated, formatDate } from '#/lib/timeFunctions';
 
 // types
-import { FeatureFlag } from "@estuary/types";
+import { FeatureFlag } from '@estuary/types';
 
 // util
-import { Link } from "wouter";
-import { useEffect, useState } from "react";
-import FeatureService from "#/services/FeatureService";
+import { Link } from 'wouter';
+import { useEffect, useState } from 'react';
+import FeatureService from '#/services/FeatureService';
 import merge from 'deepmerge';
 
 const featureService = new FeatureService();
 
-type AllEnvironmentToggles = Record<string, FeatureFlag['environmentNames']>
+type AllEnvironmentToggles = Record<string, FeatureFlag['environmentNames']>;
 export interface FeatureTableProps {
   featureFlags: FeatureFlag[];
   isLoading: boolean;
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function FeatureTable({ featureFlags, isLoading, setIsLoading }: FeatureTableProps) {
-  const [environmentToggles, setEnvironmentToggles] = useState<AllEnvironmentToggles>({});
-  const [allEnvironmentNames, setAllEnvironmentNames] = useState<Set<string>>(new Set());
+export default function FeatureTable({
+  featureFlags,
+  isLoading,
+  setIsLoading,
+}: FeatureTableProps) {
+  const [environmentToggles, setEnvironmentToggles] =
+    useState<AllEnvironmentToggles>({});
+  const [allEnvironmentNames, setAllEnvironmentNames] = useState<string[]>([]);
 
   useEffect(() => {
     const setEnvironmentStatuses = async () => {
       // todo: replace with map/list of names of all fetched environments
       const environments: AllEnvironmentToggles = {};
-      
+
       featureFlags.forEach((flag) => {
         environments[flag.name] = flag.environmentNames;
 
-        setAllEnvironmentNames((prevState) => new Set([
-          ...prevState,
-          ...Object.keys(flag.environmentNames),
-        ]));
+        setAllEnvironmentNames((prevState) =>
+          [
+            ...new Set([...prevState, ...Object.keys(flag.environmentNames)]),
+          ].toSorted(),
+        );
       });
 
       setEnvironmentToggles(environments);
@@ -51,50 +57,51 @@ export default function FeatureTable({ featureFlags, isLoading, setIsLoading }: 
 
   // console.table(featureFlags);
 
-  return (<>
-    { featureFlags.length && !isLoading &&
-      <Table.Root className="table">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>Feature Name</Table.ColumnHeader>
-            {[...allEnvironmentNames].map((envName, i) => (
-              <Table.ColumnHeader key={`${envName}-header-${i}`}>
-                {envName.charAt(0).toUpperCase() + envName.slice(1)}
-              </Table.ColumnHeader>
-            ))}
-            <Table.ColumnHeader>Default Value</Table.ColumnHeader>
-            <Table.ColumnHeader>Override Rules</Table.ColumnHeader>
-            <Table.ColumnHeader>Last Updated</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {featureFlags.map((flag: FeatureFlag) => {
-            return (
-              <FeatureFlagTableRow
-                key={flag.id}
-                environmentToggles={environmentToggles}
-                allEnvironmentNames={[...allEnvironmentNames]}
-                flag={flag}
-                setEnvironmentToggles={setEnvironmentToggles}
-              />
-            );
-          })}
-        </Table.Body>
-      </Table.Root>
-    }
-  </>);
-};
+  return (
+    <>
+      {featureFlags.length && !isLoading && (
+        <Table.Root className="table">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Feature Name</Table.ColumnHeader>
+              {[...allEnvironmentNames].map((envName, i) => (
+                <Table.ColumnHeader key={`${envName}-header-${i}`}>
+                  {envName.charAt(0).toUpperCase() + envName.slice(1)}
+                </Table.ColumnHeader>
+              ))}
+              <Table.ColumnHeader>Default Value</Table.ColumnHeader>
+              <Table.ColumnHeader>Override Rules</Table.ColumnHeader>
+              <Table.ColumnHeader>Last Updated</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {featureFlags.map((flag: FeatureFlag) => {
+              return (
+                <FeatureFlagTableRow
+                  key={flag.id}
+                  environmentToggles={environmentToggles}
+                  allEnvironmentNames={[...allEnvironmentNames]}
+                  flag={flag}
+                  setEnvironmentToggles={setEnvironmentToggles}
+                />
+              );
+            })}
+          </Table.Body>
+        </Table.Root>
+      )}
+    </>
+  );
+}
 
-
-      
-
-interface FeatureFlagTableRowProps  {
+interface FeatureFlagTableRowProps {
   environmentToggles: AllEnvironmentToggles;
   allEnvironmentNames: string[];
   flag: FeatureFlag;
-  setEnvironmentToggles: React.Dispatch<React.SetStateAction<AllEnvironmentToggles>>;
+  setEnvironmentToggles: React.Dispatch<
+    React.SetStateAction<AllEnvironmentToggles>
+  >;
 }
-  
+
 function FeatureFlagTableRow({
   environmentToggles,
   allEnvironmentNames,
@@ -103,21 +110,26 @@ function FeatureFlagTableRow({
 }: FeatureFlagTableRowProps) {
   // todo: remove this placeholder guard clause
   if (!(flag.name in environmentToggles)) {
-    throw new Error(`No flag with the specified name ${flag.name} was present in 'environmentToggles'! `);
+    throw new Error(
+      `No flag with the specified name ${flag.name} was present in 'environmentToggles'! `,
+    );
   }
 
   // impute missing environment names with `false`
   const [envStatuses, setEnvStatuses] = useState<Record<string, boolean>>(
-    allEnvironmentNames.reduce((acc, envName) => (
-      Object.assign(acc, { [envName]: envName in flag.environmentNames })
-    ), {})
+    allEnvironmentNames.reduce(
+      (acc, envName) =>
+        Object.assign(acc, { [envName]: envName in flag.environmentNames }),
+      {},
+    ),
   );
 
   const toggleEnvironmentDisplay = (envName: string) => {
-    setEnvStatuses((prevState) => (
-      { ...prevState, [envName]: !prevState[envName] }
-    ));
-  }
+    setEnvStatuses((prevState) => ({
+      ...prevState,
+      [envName]: !prevState[envName],
+    }));
+  };
 
   const handleCheckedChange = (envName: string, checked: boolean) => {
     featureService.toggleEnvironment(flag.id, envName);
@@ -125,7 +137,7 @@ function FeatureFlagTableRow({
     setEnvironmentToggles((prevState) => {
       if (checked) {
         const update = {
-          [flag.name]: { [envName]: true as const }
+          [flag.name]: { [envName]: true as const },
         };
         return merge(prevState, update);
       } else {
@@ -135,21 +147,21 @@ function FeatureFlagTableRow({
     });
 
     toggleEnvironmentDisplay(envName);
-  }
+  };
 
   return (
     <Table.Row key={flag.id}>
       <Table.Cell color="black" textDecor="none">
         <Link href={`/features/${flag.id}`}>{flag.name}</Link>
       </Table.Cell>
-      {allEnvironmentNames.map((envName: string) => 
+      {allEnvironmentNames.map((envName: string) => (
         <Table.Cell key={envName}>
           <Switch
             checked={envStatuses[envName]}
             onCheckedChange={(e) => handleCheckedChange(envName, e.checked)}
           />
         </Table.Cell>
-      )}
+      ))}
       <Table.Cell>
         <Tooltip showArrow openDelay={50} content={flag.value.type}>
           <Text
@@ -167,12 +179,9 @@ function FeatureFlagTableRow({
           openDelay={50}
           content={formatDate(Number(flag.updatedAt))}
         >
-          <Text width="fit-content">
-            {lastUpdated(Number(flag.updatedAt))}
-          </Text>
+          <Text width="fit-content">{lastUpdated(Number(flag.updatedAt))}</Text>
         </Tooltip>
       </Table.Cell>
     </Table.Row>
   );
 }
-    
