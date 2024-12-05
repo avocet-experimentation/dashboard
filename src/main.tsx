2a6afe8e7f0495453db201ce351d0701e0bc8630
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-04 18:59:37 -0800

    Move ServiceContext; install deepcopy
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ChakraUIProvider } from './components/ui/provider';
 import App from './App';
 import './assets/stylesheets/index.css';
-import { ServicesProvider } from './hooks/ServiceContext';
+import { ServicesProvider } from './services/ServiceContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
