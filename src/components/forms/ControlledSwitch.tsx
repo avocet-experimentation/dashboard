03727a797514710c257249d8612fdfc08b575520
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-04 19:02:15 -0800

    Add building blocks for controlled forms
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/components/forms/ControlledSwitch.tsx
@@ -0,0 +1,59 @@
+import { Flex, Text } from '@chakra-ui/react';
+import {
+  FieldValues,
+  Controller,
+  FieldPath,
+  useFormContext,
+} from 'react-hook-form';
+import { Switch } from '../ui/switch';
+import { Field } from '../ui/field';
+
+interface ControlledSwitchProps<
+  T extends FieldValues,
+  K extends FieldPath<T> = FieldPath<T>,
+> {
+  fieldPath: K;
+  label: string;
+  labelPosition: 'right' | 'left';
+  switchId: string;
+}
+
+/**
+ * Requires the form to be wrapped in a FormProvider
+ */
+export default function ControlledSwitch<T extends FieldValues>({
+  fieldPath,
+  label,
+  labelPosition,
+  switchId,
+}: ControlledSwitchProps<T>) {
+  const { control, formState } = useFormContext<T>();
+
+  return (
+    <Controller
+      name={fieldPath}
+      control={control}
+      render={({ field }) => (
+        <Field
+          invalid={!!formState.errors[fieldPath]}
+          errorText={formState.errors[fieldPath]?.message as string | undefined}
+        >
+          <Flex>
+            {labelPosition === 'left' && <Text marginRight="5px">{label}</Text>}
+            <Switch
+              id={switchId}
+              name={field.name}
+              checked={!!field.value}
+              onCheckedChange={({ checked }) => field.onChange(checked)}
+              onBlur={field.onBlur}
+              width="fit-content"
+            >
+              {labelPosition === 'right'
+                && 'Enabled by default for new feature flags'}
+            </Switch>
+          </Flex>
+        </Field>
+      )}
+    />
+  );
+}
