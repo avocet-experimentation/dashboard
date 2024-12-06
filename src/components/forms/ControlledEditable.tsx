7188f228dab32d01e23833674de334c11fa72b2d
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-05 18:44:23 -0800

    Add helper components: - LoaderWrapper to display a placeholder ReactNode while data is loading - ControlledEditable for managing string properties on objects
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/components/forms/ControlledEditable.tsx
@@ -0,0 +1,75 @@
+import {
+  Editable,
+  EditableValueChangeDetails,
+  Heading,
+  HStack,
+  IconButton,
+  Stack,
+} from '@chakra-ui/react';
+import { X, Check, FilePenLine } from 'lucide-react';
+import { useState } from 'react';
+
+interface ControlledEditableProps {
+  label: string;
+  initialValue: string;
+  submitHandler: (e: EditableValueChangeDetails) => Promise<string>;
+  startInEditMode?: boolean;
+  disabled?: boolean;
+}
+/**
+ * For managing string fields on objects.
+ * @param submitHandler a callback to write changes to a database,
+ * returning the new value or the previous value if writing fails
+ */
+export default function ControlledEditable({
+  label,
+  initialValue,
+  submitHandler,
+  startInEditMode = false,
+  disabled = false,
+}: ControlledEditableProps) {
+  const [value, setValue] = useState<string>(initialValue);
+
+  const handleValueCommit = async (e: EditableValueChangeDetails) => {
+    const submitResult = await submitHandler(e);
+    setValue(submitResult);
+  };
+
+  return (
+    <Editable.Root
+      disabled={disabled}
+      value={value}
+      defaultEdit={startInEditMode}
+      selectOnFocus={false}
+      activationMode="click"
+      onValueChange={(e) => setValue(e.value)}
+      onValueCommit={(e) => handleValueCommit(e)}
+      submitMode="enter"
+    >
+      <Stack padding="15px" bg="white" borderRadius="5px" width="full">
+        <HStack gap={2.5}>
+          <Heading size="lg">{label}</Heading>
+          <Editable.Control>
+            <Editable.EditTrigger asChild>
+              <IconButton variant="ghost" size="xs">
+                <FilePenLine color="black" />
+              </IconButton>
+            </Editable.EditTrigger>
+            <Editable.CancelTrigger asChild>
+              <IconButton variant="outline" size="xs">
+                <X />
+              </IconButton>
+            </Editable.CancelTrigger>
+            <Editable.SubmitTrigger asChild>
+              <IconButton variant="outline" size="xs">
+                <Check />
+              </IconButton>
+            </Editable.SubmitTrigger>
+          </Editable.Control>
+        </HStack>
+        <Editable.Preview minH="48px" alignItems="flex-start" width="full" />
+        <Editable.Textarea />
+      </Stack>
+    </Editable.Root>
+  );
+}
