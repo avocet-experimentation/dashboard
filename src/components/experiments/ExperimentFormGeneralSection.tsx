472353c0adbdc5ce53b4797f3b1aabec344e5ee7
Adnan Shihabi <adnan@ashihabi.me>
2024-12-06 19:30:34 -0800

    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com> Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com> Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

---
+++ src/components/experiments/ExperimentFormGeneralSection.tsx
@@ -0,0 +1,21 @@
+import { Stack } from '@chakra-ui/react';
+import {
+  DescriptionField,
+  HypothesisField,
+  NameField,
+} from '../forms/DefinedFields';
+
+function ExperimentFormGeneralSection() {
+  return (
+    <Stack gap="4">
+      <NameField
+        label="Experiment Name"
+        helperText="Acts as a unique identifier used to track impressions and analyze results."
+      />
+      <DescriptionField />
+      <HypothesisField />
+    </Stack>
+  );
+}
+
+export default ExperimentFormGeneralSection;
---
+++ src/components/experiments/LinkFeatureForm copy.tsx
@@ -0,0 +1,52 @@
+import { Controller, useForm } from "react-hook-form";
+import { Field } from "../ui/field";
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "../ui/select";
+import { chakra } from "@chakra-ui/react";
+
+const LinkFeatureForm = ({ formId, setIsLoading }) => {
+  return (
+    <chakra.form id={formId}>
+      <Field
+        label="Assign value based on attribute"
+        helperText="Will be hashed together with the Tracking Key to determine which variation to assign."
+      >
+        <Controller
+          control={control}
+          name="featureFlag"
+          render={({ field }) => {
+            return (
+              <SelectRoot
+                name={field.name}
+                value={field.value}
+                onValueChange={({ value }) => {
+                  field.onChange(value[0]);
+                }}
+                onInteractOutside={() => field.onBlur()}
+                collection={allFeatures}
+              >
+                <SelectTrigger>
+                  <SelectValueText />
+                </SelectTrigger>
+                <SelectContent>
+                  {allFeatures.items.map((feature) => (
+                    <SelectItem item={feature.name} key={feature.id}>
+                      {feature.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectRoot>
+            );
+          }}
+        />
+      </Field>
+    </chakra.form>
+  );
+};
+
+export default LinkFeatureForm;
