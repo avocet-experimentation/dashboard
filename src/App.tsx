f52a392916e32149225c216e529c6e2435553f18
Adnan Shihabi <adnan@ashihabi.me>
2024-12-11 00:13:52 -0800

    `App` component change:
    Change vertical component structure to `Grid` to ensure `TopBar` doesn't move when below components change size.
    
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Grid, GridItem } from '@chakra-ui/react';
 import { Route, Switch } from 'wouter';
-// import "./assets/stylesheets/App.css";
 import Navbar from './components/Navbar';
 
 import FeatureFlagsMain from './components/feature-flags/FeatureFlagsMain';
@@ -15,17 +14,21 @@ export default function App() {
   return (
     <Flex direction="row" width="100vw" height="100vh" overflow="hidden">
       <Navbar />
-      <Flex direction="column" width="calc(100% - 250px)" bg="whitesmoke">
-        <TopBar />
-        <Switch>
-          <Route path="/features" component={FeatureFlagsMain} />
-          <Route path="/features/:id" component={FeatureFlagManagementPage} />
-          <Route path="/environments" component={EnvironmentsMainPage} />
-          <Route path="/experiments" component={ExperimentsMain} />
-          <Route path="/experiments/:id" component={ExperimentPage} />
-          <Route path="/telemetry" component={TelemetryMain} />
-        </Switch>
-      </Flex>
+      <Grid bg="whitesmoke" templateRows="60px 1fr" width="100%" height="100vh">
+        <GridItem>
+          <TopBar />
+        </GridItem>
+        <GridItem>
+          <Switch>
+            <Route path="/features" component={FeatureFlagsMain} />
+            <Route path="/features/:id" component={FeatureFlagManagementPage} />
+            <Route path="/environments" component={EnvironmentsMainPage} />
+            <Route path="/experiments" component={ExperimentsMain} />
+            <Route path="/experiments/:id" component={ExperimentPage} />
+            <Route path="/telemetry" component={TelemetryMain} />
+          </Switch>
+        </GridItem>
+      </Grid>
     </Flex>
   );
 }
