3375be296e54f14cb3e37958df058d95ab5633fb
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-16 14:51:53 -0800

    Create `ControlledSelect` component for easily adding controlled dropdowns to forms
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/components/forms/ControlledSelect.tsx
@@ -0,0 +1,96 @@
+import {
+  createListCollection,
+  ListCollection,
+  SelectValueChangeDetails,
+} from '@chakra-ui/react';
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from '#/components/ui/select';
+import { Field } from '#/components/ui/field';
+import {
+  Control,
+  Controller,
+  UseFormSetValue,
+  useFormContext,
+  FieldValues,
+  Path,
+  PathValue,
+} from 'react-hook-form';
+
+interface ControlledSelectProps<T, O> {
+  label: string;
+  fieldPath: Path<T>;
+  placeholder?: string;
+  // control: Control<FeatureFlagDraft, any>;
+  // setValue: UseFormSetValue<FeatureFlagDraft>;
+  options: O[];
+  handleValueChange?: (
+    value: SelectValueChangeDetails<O>['value'][number],
+  ) => void;
+  disabled?: boolean;
+}
+
+/**
+ * (WIP) Dropdown controlled via react-hook-form. Expects the form to be
+ * wrapped in a FormContextProvider
+ */
+export default function ControlledSelect<
+  T extends FieldValues,
+  O extends { label: string; value: PathValue<T, Path<T>> },
+>({
+  label,
+  fieldPath,
+  placeholder = 'select an option',
+  options,
+  handleValueChange,
+  disabled,
+}: ControlledSelectProps<T, O>) {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<T>();
+
+  const optionCollection = createListCollection({ items: options });
+
+  return (
+    <Field
+      label={label}
+      invalid={!!errors[fieldPath]}
+      errorText={(errors[fieldPath]?.message as string) ?? undefined}
+      width="320px"
+    >
+      <Controller
+        control={control}
+        name={fieldPath}
+        render={({ field }) => (
+          <SelectRoot
+            name={field.name}
+            value={field.value}
+            collection={optionCollection}
+            disabled={disabled}
+            onValueChange={(e: SelectValueChangeDetails<O>) => {
+              field.onChange(e.value);
+              handleValueChange?.(e.value[0]);
+            }}
+            onInteractOutside={() => field.onBlur()}
+          >
+            <SelectTrigger>
+              <SelectValueText placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent zIndex="popover">
+              {optionCollection.items.map((option) => (
+                <SelectItem item={option} key={option.label}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </SelectRoot>
+        )}
+      />
+    </Field>
+  );
+}
