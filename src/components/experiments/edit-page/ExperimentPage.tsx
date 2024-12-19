5d6711424ef2d5b4678219cab68d5d122667c4b5
Adnan Shihabi <adnan@ashihabi.me>
2024-12-18 19:36:37 -0800

    Implement experiment page refactorization.
    Note: The original contents of this file was lost during a rebase. Changes had to be reimplemented.
    
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/components/experiments/edit-page/ExperimentPage.tsx
+++ src/components/experiments/edit-page/ExperimentPage.tsx
@@ -1,128 +1,31 @@
 import ExperimentService from '#/services/ExperimentService';
-import FeatureService from '#/services/FeatureService';
 import { useLocation, useRoute } from 'wouter';
 import NotFound from '../../NotFound';
-
 import { useEffect, useState } from 'react';
 import {
-  PieChart,
-  Pie,
-  Cell,
-  ResponsiveContainer,
-  Legend,
-  Tooltip,
-  LabelProps as RechartsLabelProps,
-} from 'recharts';
-import {
   Box,
-  Editable,
+  EditableValueChangeDetails,
   Flex,
   Heading,
   HStack,
-  Icon,
   IconButton,
   Stack,
-  Table,
-  Text
 } from '@chakra-ui/react';
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from '../../ui/menu';
 import {
-  ALargeSmall,
-  Check,
   EllipsisVertical,
-  FilePenLine,
-  Hash,
-  Link,
-  LucideProps,
   OctagonX,
-  Play,
   Power,
-  ToggleLeft,
   Trash2,
-  TriangleAlert,
-  X,
 } from 'lucide-react';
-import FormModal from '../../forms/FormModal';
-import LinkFeatureForm from './LinkFeatureForm';
-import {
-  AccordionItem,
-  AccordionItemContent,
-  AccordionItemTrigger,
-  AccordionRoot,
-} from '../../ui/accordion';
-import { Experiment, ExperimentGroup, FeatureFlag } from '@estuary/types';
+import { Experiment } from '@avocet/core';
 import { Button } from '../../ui/button';
-import { Tag } from '../../ui/tag';
-import { Tooltip as ChakraTooltip } from '../../ui/tooltip';
-
-interface CustomLabelProps extends RechartsLabelProps {
-  innerRadius: number; // The inner radius of the pie slice
-  outerRadius: number; // The outer radius of the pie slice
-  cx: number; // The x-coordinate of the center of the chart
-  cy: number; // The y-coordinate of the center of the chart
-  midAngle: number; // The angle (in degrees) at the middle of the pie slice
-  percent: number; // The percentage of the pie slice relative to the total
-}
+import VariationGroups from './VariationGroupsSection';
+import LinkedFeatures from './LinkedFeaturesSection';
+import ControlledEditable from '#/components/forms/ControlledEditable';
+import { ExperimentDraft } from '@avocet/core';
 
 const experimentService = new ExperimentService();
-const featureService = new FeatureService();
-
-const LINK_FEATURE_FORM = 'link-feature-form';
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-}: CustomLabelProps) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? 'start' : 'end'}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-const formatGroups = (groups) => {
-  return groups.map((group) => ({
-    name: group.name,
-    value: group.proportion * 1000,
-  }));
-};
-
-const renderValueTypeIcon = (valueType: string) => {
-  switch(valueType) {
-    case "string":
-      return <ALargeSmall/>
-    case "number":
-      return <Hash/>
-    case "boolean":
-      return <ToggleLeft/>
-    default:
-      return <></>
-  }
-}
-
-const flagTagProperties = (expEnvironment: string, flagEnvironments: string[]) => {
-  const environmentEnabled = Object.keys(flagEnvironments).includes(expEnvironment);
-  const icon = environmentEnabled ? <Play /> : <TriangleAlert />;
-  const text = environmentEnabled ? "Live" : "Disabled";
-  const colorPalette = environmentEnabled ? "green" : "red";
-  const tooltip = environmentEnabled ? `This feature has the ${expEnvironment} environment enabled.` : `The ${expEnvironment} environment is disabled for this feature, so the experiment is not active`
-  return { icon, text, colorPalette, tooltip };
-}
 
 const experimentButtonProperties = (expStatus: string, expId: string) => {
   const active = expStatus === "active";
@@ -133,20 +36,9 @@ const experimentButtonProperties = (expStatus: string, expId: string) => {
   return { icon, text, colorPalette, onClick };
 }
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
 const ExperimentPage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [editDesc, setEditDesc] = useState<boolean>(false);
-  const [editHypo, setEditHypo] = useState<boolean>(false);
   const [experiment, setExperiment] = useState<Experiment | null>(null);
-  const [linkedFeatures, setLinkedFeatures] = useState<Partial<FeatureFlag>[]>([]);
   const [match, params] = useRoute('/experiments/:id');
   const [location, navigate] = useLocation();
 
@@ -168,29 +60,16 @@ const ExperimentPage = () => {
     handleGetExperiment();
   }, []);
 
-  useEffect(() => {
-    const handleGetLinkedFeatures = async () => {
-      if (experiment) {
-        try {
-          const features: Partial<FeatureFlag>[] = [];
-          for (const flagId of experiment.flagIds) {
-            const flag = await featureService.getFeature(flagId);
-            if (flag.ok) {
-              features.push({
-                id: flag.body.id,
-                name: flag.body.name,
-                environmentNames: flag.body.environmentNames,
-                valueType: flag.body.value.type,
-              });
-            }
-          }
-          setLinkedFeatures(features);
-        } catch (error) {}
-      }
+  const handleExperimentUpdate = async (
+      updates: Partial<ExperimentDraft>,
+    ): Promise<boolean> => {
+      const response = await experimentService.updateExperiment(
+        params.id,
+        updates,
+      );
+  
+      return response.ok;
     };
-
-    handleGetLinkedFeatures();
-  }, [experiment]);
 
   const handleDeleteClick = async () => {
     if (!experiment) return;
@@ -242,84 +121,22 @@ const ExperimentPage = () => {
             Overview
           </Heading>
           <Stack gap={4}>
-            <Stack padding="15px" bg="white" borderRadius="5px">
-              <HStack gap={2.5}>
-                <Heading size="lg">Description</Heading>
-                <Icon
-                  size="sm"
-                  cursor="pointer"
-                  onClick={() => setEditDesc(true)}
-                >
-                  <FilePenLine color="black" />
-                </Icon>
-              </HStack>
-              <Editable.Root
-                defaultValue={experiment.description || undefined}
-                edit={editDesc}
-                activationMode="focus"
-                onBlur={() => {
-                  // setEditDesc(false);
-                }}
-              >
-                <Editable.Preview
-                  minH="48px"
-                  alignItems="flex-start"
-                  width="full"
-                />
-                <Editable.Textarea />
-                <Editable.Control>
-                  <Editable.CancelTrigger asChild>
-                    <IconButton variant="outline" size="xs">
-                      <X />
-                    </IconButton>
-                  </Editable.CancelTrigger>
-                  <Editable.SubmitTrigger asChild>
-                    <IconButton variant="outline" size="xs">
-                      <Check />
-                    </IconButton>
-                  </Editable.SubmitTrigger>
-                </Editable.Control>
-              </Editable.Root>
-            </Stack>
-            <Stack padding="15px" bg="white" borderRadius="5px">
-              <HStack gap={2.5}>
-                <Heading size="lg">Hypothesis</Heading>
-                <Icon
-                  size="sm"
-                  cursor="pointer"
-                  onClick={() => setEditHypo(true)}
-                >
-                  <FilePenLine color="black" />
-                </Icon>
-              </HStack>
-              <Editable.Root
-                defaultValue={experiment.hypothesis || undefined}
-                edit={editHypo}
-                activationMode="focus"
-                onBlur={() => {
-                  // setEditDesc(false);
-                }}
-              >
-                <Editable.Preview
-                  minH="48px"
-                  alignItems="flex-start"
-                  width="full"
-                />
-                <Editable.Textarea />
-                <Editable.Control>
-                  <Editable.CancelTrigger asChild>
-                    <IconButton variant="outline" size="xs">
-                      <X />
-                    </IconButton>
-                  </Editable.CancelTrigger>
-                  <Editable.SubmitTrigger asChild>
-                    <IconButton variant="outline" size="xs">
-                      <Check />
-                    </IconButton>
-                  </Editable.SubmitTrigger>
-                </Editable.Control>
-              </Editable.Root>
-            </Stack>
+            <ControlledEditable label="Description" initialValue={experiment.description ?? ''} 
+              submitHandler={async (e: EditableValueChangeDetails) => {
+                const success = await handleExperimentUpdate({
+                  description: e.value,
+                });
+                return success ? e.value : (experiment.description ?? '');
+              }}
+            />
+            <ControlledEditable label="Hypothesis" initialValue={experiment.description ?? ''} 
+              submitHandler={async (e: EditableValueChangeDetails) => {
+                const success = await handleExperimentUpdate({
+                  hypothesis: e.value,
+                }); 
+                return success ? e.value : (experiment.hypothesis ?? '');
+              }}
+            />
           </Stack>
         </Box>
         <Box>
@@ -327,44 +144,13 @@ const ExperimentPage = () => {
             Implementation
           </Heading>
           <Stack gap={4}>
-            <Stack padding="15px" bg="white" borderRadius="5px">
-              <Heading size="lg">Variation Groups</Heading>
-              <ResponsiveContainer minWidth="200px" minHeight="200px">
-                <PieChart width={400} height={400}>
-                  <Pie
-                    data={experiment.groups}
-                    dataKey="proportion"
-                    nameKey="name"
-                    cx="50%"
-                    cy="50%"
-                    labelLine={false}
-                    label={renderCustomizedLabel}
-                    outerRadius={80}
-                    fill="#8884d8"
-                  >
-                    {data.map((_, index) => (
-                      <Cell
-                        cursor="help"
-                        key={`cell-${index}`}
-                        fill={COLORS[index % COLORS.length]}
-                      />
-                    ))}
-                  </Pie>
-                  <Legend
-                    layout="vertical"
-                    align="right"
-                    verticalAlign="middle"
-                  />
-                  <Tooltip />
-                </PieChart>
-              </ResponsiveContainer>
-            </Stack>
+            <VariationGroups experiment={experiment} />
             <Stack padding="15px" bg="white" borderRadius="5px">
               <Flex justifyContent="space-between">
                 <Heading size="lg">
                   Linked Features ({experiment.flagIds.length})
                 </Heading>
-                <FormModal
+                {/* <FormModal
                   triggerButtonIcon={<Link />}
                   triggerButtonText={'Link Feature Flag'}
                   title={`Link Feature to ${experiment.name}`}
@@ -375,55 +161,9 @@ const ExperimentPage = () => {
                     formId={LINK_FEATURE_FORM}
                     setIsLoading={undefined}
                   />
-                </FormModal>
+                </FormModal> */}
               </Flex>
-              <AccordionRoot variant="enclosed" multiple>
-                {linkedFeatures.map((feature) => {
-                  console.log(feature.environmentNames)
-                  const statusTagProperties = flagTagProperties(experiment.environmentName, feature.environmentNames)
-                  return (
-                  <AccordionItem key={feature.id} value={feature.name}>
-                    <AccordionItemTrigger id={feature.id}>
-                      <Stack direction="row" gap={4}>
-                      <Text>{feature.name}</Text>
-                      <Tag size="md" variant="outline" startElement={renderValueTypeIcon(feature.valueType)}>{feature.valueType}</Tag>
-                      <ChakraTooltip
-                        showArrow
-                        openDelay={50}
-                        content={statusTagProperties.tooltip}
-                      >
-                        <Tag size="md" startElement={statusTagProperties.icon} colorPalette={statusTagProperties.colorPalette}>{statusTagProperties.text}</Tag>
-                      </ChakraTooltip>
-                      </Stack>
-                    </AccordionItemTrigger>
-                    <AccordionItemContent>
-                      <Stack gap={2}>
-                        <Text>Feature Values</Text>
-                        <Table.Root width="250px" size="sm" variant="outline" showColumnBorder>
-                          <Table.Header>
-                            <Table.Row>
-                              <Table.ColumnHeader>GROUP</Table.ColumnHeader>
-                              <Table.ColumnHeader>SERVING</Table.ColumnHeader>
-                            </Table.Row>
-                          </Table.Header>
-                          <Table.Body>
-                            {experiment.groups.map((group: ExperimentGroup) => {
-                              const sequence = group.sequence[0];
-                              const servedValue = experiment.definedTreatments[sequence].flagStates.find((treatmentFeature) => treatmentFeature.id === feature.id).value
-                              return (
-                                <Table.Row key={group.id}>
-                                  <Table.Cell>{group.name}</Table.Cell>
-                                  <Table.Cell>{servedValue}</Table.Cell>
-                                </Table.Row>
-                              )
-                            })}
-                          </Table.Body>
-                        </Table.Root>
-                      </Stack>
-                    </AccordionItemContent>
-                  </AccordionItem>
-                )})}
-              </AccordionRoot>
+              <LinkedFeatures experiment={experiment} />
             </Stack>
           </Stack>
         </Box>
