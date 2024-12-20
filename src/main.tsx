30b84cc77369c8575acae1eee2cbd8e4bba72be8
Adnan Shihabi <adnan@ashihabi.me>
2024-12-20 14:35:15 -0800

    Add `auth0` login page and front-end auth check
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraUIProvider } from './components/ui/provider';
 import App from './App';
-import './assets/stylesheets/index.css';
+// import './assets/stylesheets/index.css';
 import { ServicesProvider } from './services/ServiceContext';
 import { EnvironmentProvider } from './lib/EnvironmentContext';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const authDomain = import.meta.env.AUTH0_DOMAIN; // currently returning undefined
+const authClientId = import.meta.env.AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ServicesProvider>
       <EnvironmentProvider>
         <ChakraUIProvider>
-          <App />
+          <Auth0Provider domain={authDomain} clientId={authClientId} authorizationParams={{ redirect_uri: window.location.origin }}>
+            <App />
+          </Auth0Provider>
         </ChakraUIProvider>
       </EnvironmentProvider>
     </ServicesProvider>
