71375a6375cc5f06b70d4961f1f296d5c0861643
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-20 19:59:49 -0800

    Delete redundant `ExperimentPage`
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,33 @@ import Navbar from './components/Navbar';
 import FeatureFlagsMain from './components/feature-flags/FeatureFlagsMain';
 import ExperimentsMain from './components/experiments/ExperimentsMain';
 import FeatureFlagManagementPage from './components/feature-flags/edit-page/FeatureFlagManagementPage';
-import ExperimentPage from './components/experiments/edit-page/ExperimentPage';
 import EnvironmentsMainPage from './components/environments/EnvironmentsMainPage';
 import TelemetryMain from './telemetry/TelemetryMain';
+import ExperimentManagementPage from './components/experiments/edit-page/ExperimentManagementPage';
 
 export default function App() {
   return (
-      <Grid bg="whitesmoke" templateColumns="300px 1fr" templateRows="1fr" width="100vw" height="100vh" overflow="hidden">
-        <GridItem display="flex"  flexDir="row" justifyContent="center">
-          <Navbar />
-        </GridItem>
-        <GridItem>
-          <Switch>
-            <Route path="/features" component={FeatureFlagsMain} />
-            <Route path="/features/:id" component={FeatureFlagManagementPage} />
-            <Route path="/environments" component={EnvironmentsMainPage} />
-            <Route path="/experiments" component={ExperimentsMain} />
-            <Route path="/experiments/:id" component={ExperimentPage} />
-            <Route path="/telemetry" component={TelemetryMain} />
-          </Switch>
-        </GridItem>
-      </Grid>
+    <Grid
+      bg="whitesmoke"
+      templateColumns="300px 1fr"
+      templateRows="1fr"
+      width="100vw"
+      height="100vh"
+      overflow="hidden"
+    >
+      <GridItem display="flex" flexDir="row" justifyContent="center">
+        <Navbar />
+      </GridItem>
+      <GridItem>
+        <Switch>
+          <Route path="/features" component={FeatureFlagsMain} />
+          <Route path="/features/:id" component={FeatureFlagManagementPage} />
+          <Route path="/environments" component={EnvironmentsMainPage} />
+          <Route path="/experiments" component={ExperimentsMain} />
+          <Route path="/experiments/:id" component={ExperimentManagementPage} />
+          <Route path="/telemetry" component={TelemetryMain} />
+        </Switch>
+      </GridItem>
+    </Grid>
   );
 }
