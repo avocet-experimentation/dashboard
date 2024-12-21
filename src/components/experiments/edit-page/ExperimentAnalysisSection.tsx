52fdc964f71d2624ae2eae84b1457ebd806d14c7
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-20 20:02:08 -0800

    Add WIP reusable `PageSelect` and `ExperimentAnalysisSection` components
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/components/experiments/edit-page/ExperimentAnalysisSection.tsx
@@ -0,0 +1,62 @@
+import PageSelect from '#/components/forms/PageSelect';
+import { ServicesContext } from '#/services/ServiceContext';
+import TelemetryTypeSelector from '#/telemetry/TelemetryTypeSelector';
+import { Experiment, Metric } from '@avocet/core';
+import { Box, createListCollection } from '@chakra-ui/react';
+import { useContext, useEffect, useState } from 'react';
+
+interface SpanTypeListItem {
+  label: string;
+  value: string | null;
+}
+
+interface ExperimentAnalysisSectionProps {
+  experiment: Experiment;
+}
+
+/**
+ * (WIP) For setting the variable of interest and
+ * viewing data
+ */
+export default function ExperimentAnalysisSection({
+  experiment,
+}: ExperimentAnalysisSectionProps) {
+  const [depVars, setDepVars] = useState<Metric[]>(experiment.dependents);
+  console.log('dependent variables:');
+  console.table(depVars);
+
+  return (
+    <Box>
+      <ExperimentDependentSelector />
+    </Box>
+  );
+}
+
+function ExperimentDependentSelector() {
+  const [allSpanTypes, setAllSpanTypes] = useState<SpanTypeListItem[]>([]);
+
+  const services = useContext(ServicesContext);
+
+  const fetchSpanTypes = async () => {
+    const response = await services.telemetry.getAllSpanTypes();
+    if (response.ok) {
+      // const spanTypeCollection = createListCollection<SpanTypeListItem>({
+      //   items: [
+      //     { label: 'all', value: null },
+      //     ...response.body.map((type) => ({ label: type, value: type })),
+      //   ],
+      // });
+
+      setAllSpanTypes([
+        { label: 'all', value: null },
+        ...response.body.map((type) => ({ label: type, value: type })),
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    fetchSpanTypes;
+  }, []);
+
+  return <PageSelect options={allSpanTypes} />;
+}
