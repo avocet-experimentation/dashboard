650a28a34164386627f2a42c6226d32a1e0bae34
Adnan Shihabi <adnan@ashihabi.me>
2024-12-24 15:51:23 -0800

    add: Auth0 variable encapsulation and ChakraUI theming/coloring
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraUIProvider } from './components/ui/provider';
 import App from './App';
-// import './assets/stylesheets/index.css';
 import { ServicesProvider } from './services/ServiceContext';
 import { EnvironmentProvider } from './lib/EnvironmentContext';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { system } from "./theme"
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ColorModeProvider } from './components/ui/color-mode';
 
-const authDomain = import.meta.env.AUTH0_DOMAIN; // currently returning undefined
-const authClientId = import.meta.env.AUTH0_CLIENT_ID;
+const authDomain = import.meta.env.VITE_AUTH0_DOMAIN;
+const authClientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const authAudience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ServicesProvider>
       <EnvironmentProvider>
-        <ChakraUIProvider>
-          <Auth0Provider domain={authDomain} clientId={authClientId} authorizationParams={{ redirect_uri: window.location.origin }}>
-            <App />
-          </Auth0Provider>
-        </ChakraUIProvider>
+        <ChakraProvider theme={defaultSystem} value={system}>
+          <ColorModeProvider>
+            <Auth0Provider domain={authDomain} clientId={authClientId}
+            authorizationParams={{
+              redirect_uri: window.location.origin,
+              audience: authAudience,
+            }}
+            > 
+              <App />
+            </Auth0Provider>
+          </ColorModeProvider>
+        </ChakraProvider>
       </EnvironmentProvider>
     </ServicesProvider>
   </React.StrictMode>,
