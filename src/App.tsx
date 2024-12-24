42658845a13e44915460ac4af960a7594d503a62
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-23 19:49:42 -0800

    `App` component: Format and standardize
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,28 @@ import SDKConnectionsMain from './components/sdk-connections/SDKConnectionsMain'
 
 export default function App() {
   return (
-      <Grid bg="whitesmoke" templateColumns="300px 1fr" templateRows="1fr" width="100vw" height="100vh" overflow="hidden">
-        <GridItem display="flex"  flexDir="row" justifyContent="center">
-          <Navbar />
-        </GridItem>
-        <GridItem>
-          <Switch>
-            <Route path="/features" component={FeatureFlagsMain} />
-            <Route path="/features/:id" component={FeatureFlagManagementPage} />
-            <Route path="/environments" component={EnvironmentsMainPage} />
-            <Route path="/experiments" component={ExperimentsMain} />
-            <Route path="/connections" component={SDKConnectionsMain} />
-            <Route path="/experiments/:id" component={ExperimentPage} />
-            <Route path="/telemetry" component={TelemetryMain} />
-          </Switch>
-        </GridItem>
-      </Grid>
+    <Grid
+      bg="whitesmoke"
+      templateColumns="300px 1fr"
+      templateRows="1fr"
+      width="100vw"
+      height="100vh"
+      overflow="hidden"
+    >
+      <GridItem display="flex" flexDir="row" justifyContent="center">
+        <Navbar />
+      </GridItem>
+      <GridItem>
+        <Switch>
+          <Route path="/features" component={FeatureFlagsMain} />
+          <Route path="/features/:id" component={FeatureFlagManagementPage} />
+          <Route path="/environments" component={EnvironmentsMainPage} />
+          <Route path="/experiments" component={ExperimentsMain} />
+          <Route path="/experiments/:id" component={ExperimentPage} />
+          <Route path="/telemetry" component={TelemetryMain} />
+          <Route path="/connections" component={SDKConnectionsMain} />
+        </Switch>
+      </GridItem>
+    </Grid>
   );
 }
