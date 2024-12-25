3781ac03593069cdf20348faee8966761c78cba5
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-24 18:59:57 -0800

    Install `graphql-request plugin` for `graphql-codegen`
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/lib/flagQueries.ts
+++ src/lib/flagQueries.ts
@@ -1,24 +1,4 @@
 import { graphql } from '#/graphql';
-// import { gql } from 'graphql-request';
-
-// export const allFlagsQueryConcise = gql`
-//   query allFeatureFlags($limit: Int, $offset: Int) {
-//     allFeatureFlags(limit: $limit, offset: $offset) {
-//       id
-//       createdAt
-//       updatedAt
-//       name
-//       description
-//       value
-//       environmentNames
-//       overrideRules {
-//         id
-//         type
-//         description
-//       }
-//     }
-//   }
-// `;
 
 export const allFlagsQuery = graphql(`
   query allFeatureFlags($limit: Int, $offset: Int) {
