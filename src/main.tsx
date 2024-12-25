d9190cffa9294916f3d21779e390049bf4c5e67a
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-24 18:43:09 -0800

    Uninstall `apollo-client`
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import App from './App';
 import './assets/stylesheets/index.css';
 import { ServicesProvider } from './services/ServiceContext';
 import { EnvironmentProvider } from './lib/EnvironmentContext';
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './lib/graphql';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -14,15 +12,13 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ApolloProvider client={apolloClient}>
-        <ServicesProvider>
-          <EnvironmentProvider>
-            <ChakraUIProvider>
-              <App />
-            </ChakraUIProvider>
-          </EnvironmentProvider>
-        </ServicesProvider>
-      </ApolloProvider>
+      <ServicesProvider>
+        <EnvironmentProvider>
+          <ChakraUIProvider>
+            <App />
+          </ChakraUIProvider>
+        </EnvironmentProvider>
+      </ServicesProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
