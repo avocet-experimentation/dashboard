4c07b4d2cf8832b4952678d42e90407be54bcd4d
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-31 14:28:57 -0800

    `EnvironmentManagementModal`: - revise to use react-query native hooks - refactor and consolidate into one file
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/components/environments/management-form/EnvironmentManagementModal.tsx
+++ src/components/environments/management-form/EnvironmentManagementModal.tsx
@@ -1,8 +1,27 @@
 import { CirclePlus, CircleEllipsis } from 'lucide-react';
 import { useState } from 'react';
-import { Environment } from '@avocet/core';
-import FormModal from '../../forms/FormModal';
-import EnvironmentManagementForm from './EnvironmentManagementForm';
+import { Stack } from '@chakra-ui/react';
+import {
+  useForm,
+  SubmitHandler,
+  FormProvider,
+  useFormContext,
+} from 'react-hook-form';
+import {
+  Environment,
+  EnvironmentDraft,
+  environmentDraftSchema,
+  SchemaParseError,
+} from '@avocet/core';
+import FormModal from '#/components/forms/FormModal';
+import { NameField } from '#/components/forms/DefinedFields';
+import ControlledSwitch from '#/components/forms/ControlledSwitch';
+import {
+  CREATE_ENVIRONMENT,
+  UPDATE_ENVIRONMENT,
+} from '#/lib/environment-queries';
+import { useMutation } from '@tanstack/react-query';
+import { gqlRequest } from '#/lib/graphql-queries';
 
 const ENVIRONMENT_MANAGEMENT_FORM_ID = 'environment-management-form';
 
@@ -36,11 +55,125 @@ export default function EnvironmentManagementModal({
       setOpen={setOpen}
       {...formModalProps}
     >
-      <EnvironmentManagementForm
-        formId={ENVIRONMENT_MANAGEMENT_FORM_ID}
-        environment={environment}
-        setOpen={setOpen}
-      />
+      {environment ? (
+        <EnvironmentManagementForm
+          environment={environment}
+          setOpen={setOpen}
+        />
+      ) : (
+        <EnvironmentCreationForm setOpen={setOpen} />
+      )}
     </FormModal>
+  );
+}
+
+/**
+ * Create a new Environment
+ */
+function EnvironmentCreationForm({
+  setOpen,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const createEnv = useMutation({
+    mutationFn: async (newEntry: EnvironmentDraft) =>
+      gqlRequest(CREATE_ENVIRONMENT, { newEntry }),
+    mutationKey: ['allEnvironments'],
+    onSuccess: () => setOpen(false),
+    onError: (e: Error) => console.error(e), // TODO: handle errors better
+  });
+
+  const formMethods = useForm<Environment>({
+    defaultValues: EnvironmentDraft.template({ name: '' }),
+  });
+
+  const onSubmit: SubmitHandler<EnvironmentDraft> = (environmentContent) => {
+    const safeParseResult =
+      environmentDraftSchema.safeParse(environmentContent);
+    if (!safeParseResult.success) {
+      // the error pretty-prints the Zod parse error message
+      throw new SchemaParseError(safeParseResult);
+    }
+
+    createEnv.mutate(safeParseResult.data);
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <EnvironmentFormFields editing={false} handleFormSubmit={onSubmit} />
+    </FormProvider>
+  );
+}
+
+interface EnvironmentManagementFormProps {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  environment: Environment;
+}
+
+/**
+ * Update an existing environment
+ */
+function EnvironmentManagementForm({
+  setOpen,
+  environment,
+}: EnvironmentManagementFormProps) {
+  const updateEnv = useMutation({
+    mutationFn: async (partialEntry: Partial<Environment>) =>
+      gqlRequest(UPDATE_ENVIRONMENT, {
+        partialEntry: { ...partialEntry, id: environment.id },
+      }),
+    mutationKey: ['allEnvironments'],
+    onSuccess: () => setOpen(false),
+    onError: (e: Error) => console.error(e), // TODO: handle errors better
+  });
+
+  const formMethods = useForm<Environment>({
+    defaultValues: environment,
+  });
+
+  const onSubmit: SubmitHandler<EnvironmentDraft> = (formContent) => {
+    const safeParseResult = environmentDraftSchema.safeParse(formContent);
+    if (!safeParseResult.success) throw new SchemaParseError(safeParseResult);
+
+    updateEnv.mutate(safeParseResult.data);
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <EnvironmentFormFields editing={true} handleFormSubmit={onSubmit} />
+    </FormProvider>
+  );
+}
+
+function EnvironmentFormFields({
+  editing,
+  handleFormSubmit,
+}: {
+  editing: boolean;
+  handleFormSubmit: SubmitHandler<EnvironmentDraft>;
+}) {
+  const { handleSubmit } = useFormContext<EnvironmentDraft>();
+
+  return (
+    <form
+      id={ENVIRONMENT_MANAGEMENT_FORM_ID}
+      onSubmit={handleSubmit(handleFormSubmit)}
+    >
+      <Stack gap="4">
+        <NameField disabled={editing} label="Environment Name" />
+        <ControlledSwitch
+          fieldPath="defaultEnabled"
+          label="Enabled by default for new feature flags"
+          labelPosition="right"
+          switchId="set-default-enabled"
+        />
+        <ControlledSwitch
+          fieldPath="pinToLists"
+          label="Display a quick toggler on the feature flags list"
+          labelPosition="right"
+          switchId="set-pin-to-lists"
+        />
+      </Stack>
+    </form>
   );
 }
