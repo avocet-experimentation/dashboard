ac60c61fe88ae61d58b8c017893b0fdba73fb168
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-31 17:32:27 -0800

    Delete obsolete query hooks and GraphQLFetchWrapper
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/lib/graphql-queries.ts
+++ src/lib/graphql-queries.ts
@@ -1,4 +1,3 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import request, { RequestOptions, Variables } from 'graphql-request';
 import { TypedDocumentNode } from 'msw/core/graphql';
 
@@ -10,11 +9,7 @@ export * from './sdk-connection-queries.ts';
 /**
  * Get a query or mutation function to pass into react-query
  */
-export const getRequestFunc = <
-  T = unknown,
-  V extends Variables = Variables,
-  // H extends RequestOptions['requestHeaders'] = RequestOptions['requestHeaders'],
->(
+export const getRequestFunc = <T = unknown, V extends Variables = Variables>(
   document: TypedDocumentNode<T, V>,
   variables: RequestOptions<V, T>['variables'],
   options?: Omit<RequestOptions<V, T>, 'document' | 'variables'>,
@@ -25,11 +20,7 @@ export const getRequestFunc = <
 /**
  * Wrapper for graphql-request
  */
-export const gqlRequest = <
-  T = unknown,
-  V extends Variables = Variables,
-  // H extends RequestOptions['requestHeaders'] = RequestOptions['requestHeaders'],
->(
+export const gqlRequest = <T = unknown, V extends Variables = Variables>(
   document: TypedDocumentNode<T, V>,
   variables: RequestOptions<V, T>['variables'],
   options?: Omit<RequestOptions<V, T>, 'document' | 'variables'>,
@@ -41,68 +32,3 @@ export const gqlRequest = <
     ...options,
   });
 };
-
-/** TODO: remove once no longer in use
- *  Hook to simplify react-query useQuery calls
- */
-export function useGQLQuery<
-  T,
-  H extends RequestOptions['requestHeaders'],
-  V extends RequestOptions['variables'],
->(cacheKey: any[], query: TypedDocumentNode<T, V>, variables?: V, headers?: H) {
-  const queryData = useQuery({
-    queryKey: cacheKey,
-    queryFn: getRequestFunc(query, variables, headers),
-  });
-
-  return queryData;
-}
-
-/** TODO: remove once no longer in use
- * Hook to simplify react-query useMutation calls
- * see https://tanstack.com/query/latest/docs/framework/react/guides/mutations
- */
-export function useGQLMutation<
-  T,
-  A,
-  H extends RequestOptions['requestHeaders'],
-  V extends Variables = Variables,
->({
-  mutation,
-  cacheKey,
-  headers,
-  onSuccess,
-  onError,
-  onSettled,
-}: {
-  mutation: TypedDocumentNode<T, V>;
-  /** Passing a key will cause its data to be invalidated upon mutation success */
-  cacheKey?: string[];
-  headers?: H;
-  onSuccess?: (data: T, variables: V, context: unknown) => void | Promise<void>;
-  onError?: (
-    error: Error,
-    variables: V,
-    context: unknown,
-  ) => void | Promise<void>;
-  onSettled?: (
-    data: unknown,
-    error: Error | null,
-    variables: V,
-    context: unknown,
-  ) => void;
-}) {
-  const queryClient = useQueryClient();
-
-  const mutationData = useMutation({
-    mutationFn: (args: V) => {
-      return gqlRequest<T, V>(mutation, args, { requestHeaders: headers });
-    },
-    async onSuccess(data: T, variables: V, context: unknown) {
-      await onSuccess?.(data, variables, context);
-    },
-    onError,
-    onSettled,
-  });
-  return mutationData;
-}
