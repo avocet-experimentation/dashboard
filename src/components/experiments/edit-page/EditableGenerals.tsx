254842598e89bd283de970f5776ae88a1758be3a
Adnan Shihabi <adnan@ashihabi.me>
2024-12-31 20:40:06 -0800

    experiments: Generic editor component for single line inputs.
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/components/experiments/edit-page/EditableGenerals.tsx
@@ -0,0 +1,30 @@
+import { Editable, HStack, Text } from '@chakra-ui/react';
+
+export function EditableGenerals({
+  fieldName = '',
+  includeName = false,
+  defaultValue,
+  inputType,
+  onValueCommit,
+}: {
+  fieldName?: string;
+  includeName?: boolean;
+  defaultValue: string;
+  inputType: 'text' | 'number';
+  onValueCommit: any;
+}) {
+  return (
+    <HStack gap={2} alignItems="center">
+      {includeName && <Text>{fieldName}:</Text>}
+      <Editable.Root
+        activationMode="dblclick"
+        defaultValue={defaultValue}
+        fontSize="inherit"
+        onValueCommit={onValueCommit}
+      >
+        <Editable.Preview />
+        <Editable.Input type={inputType} />
+      </Editable.Root>
+    </HStack>
+  );
+}
