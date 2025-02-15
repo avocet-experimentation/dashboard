81eacc8a04d8c01a7f32344345a721ece34bf576
Adnan Shihabi <adnan@ashihabi.me>
2025-02-14 19:18:54 -0800

    Configuration changes:
    Because the browser is unable to resolve the docker container name as the URL, we will use a proxy to do that when the /graphql is sent a request.
    
    Co-authored-by: Gautam Panakkal 111669555+gpanakkal@users.noreply.github.com Co-authored-by: Sean Mentele 56010359+SeanM0124@users.noreply.github.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,48 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  preview: {
-    port: 4173,
-    strictPort: true,
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:5173',
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./setupTests.ts'],
-  },
-  build: {
-    target: "ESNext",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    preview: {
+      port: 4173,
+      strictPort: true,
+      proxy: {
+        '/graphql': {
+          target: process.env.VITE_API_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      host: true,
+      origin: 'http://0.0.0.0:5173',
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./setupTests.ts'],
+    },
+    build: {
+      target: "ESNext",
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
           }
         }
       }
-    }
-  }
-});
+    },
+  });
+}
